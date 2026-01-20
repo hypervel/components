14ab95cceb53a18294234f3e8c8e8e9760448a83
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-01-20 08:58:28 +0000

    port ConfigurationUrlParser from Laravel

---
+++ src/database/src/ConfigurationUrlParser.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Database;
+
+use Hypervel\Support\ConfigurationUrlParser as BaseConfigurationUrlParser;
+
+class ConfigurationUrlParser extends BaseConfigurationUrlParser
+{
+    //
+}
