e8f8539f076d2a4a75129ece81cb9f4b22c64e31
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-01-20 10:38:37 +0000

    port HasMany relation from Laravel

--- src/database/src/Eloquent/Relations/HasMany.php
+++ src/database/src/Eloquent/Relations/HasMany.php
@@ -4,62 +4,59 @@ declare(strict_types=1);
 
 namespace Hypervel\Database\Eloquent\Relations;
 
-use Closure;
-use Hyperf\Database\Model\Relations\HasMany as BaseHasMany;
-use Hypervel\Database\Eloquent\Relations\Concerns\WithoutAddConstraints;
-use Hypervel\Database\Eloquent\Relations\Contracts\Relation as RelationContract;
+use Hypervel\Database\Eloquent\Collection as EloquentCollection;
 
 /**
  * @template TRelatedModel of \Hypervel\Database\Eloquent\Model
- * @template TParentModel of \Hypervel\Database\Eloquent\Model
+ * @template TDeclaringModel of \Hypervel\Database\Eloquent\Model
  *
- * @implements RelationContract<TRelatedModel, TParentModel, \Hypervel\Database\Eloquent\Collection<int, TRelatedModel>>
- *
- * @method \Hypervel\Database\Eloquent\Collection<int, TRelatedModel> get(array|string $columns = ['*'])
- * @method null|TRelatedModel first(array|string $columns = ['*'])
- * @method TRelatedModel firstOrFail(array|string $columns = ['*'])
- * @method mixed|TRelatedModel firstOr(\Closure|array|string $columns = ['*'], ?\Closure $callback = null)
- * @method null|TRelatedModel find(mixed $id, array|string $columns = ['*'])
- * @method TRelatedModel findOrFail(mixed $id, array|string $columns = ['*'])
- * @method TRelatedModel findOrNew(mixed $id, array|string $columns = ['*'])
- * @method \Hypervel\Database\Eloquent\Collection<int, TRelatedModel> findMany(mixed $ids, array|string $columns = ['*'])
- * @method TRelatedModel make(array $attributes = [])
- * @method TRelatedModel create(array $attributes = [])
- * @method TRelatedModel forceCreate(array $attributes)
- * @method TRelatedModel firstOrNew(array $attributes = [], array $values = [])
- * @method TRelatedModel firstOrCreate(array $attributes = [], array $values = [])
- * @method TRelatedModel updateOrCreate(array $attributes, array $values = [])
- * @method false|TRelatedModel save(\Hypervel\Database\Eloquent\Model $model)
- * @method array saveMany(array|\Hypervel\Support\Collection $models)
- * @method \Hypervel\Database\Eloquent\Collection<int, TRelatedModel> createMany(array $records)
- * @method \Hypervel\Database\Eloquent\Builder<TRelatedModel> getQuery()
- * @method \Hypervel\Support\LazyCollection<int, TRelatedModel> lazy(int $chunkSize = 1000)
- * @method \Hypervel\Support\LazyCollection<int, TRelatedModel> lazyById(int $chunkSize = 1000, ?string $column = null, ?string $alias = null)
- * @method \Hypervel\Support\LazyCollection<int, TRelatedModel> lazyByIdDesc(int $chunkSize = 1000, ?string $column = null, ?string $alias = null)
- * @method \Hypervel\Database\Eloquent\Collection<int, TRelatedModel> getResults()
+ * @extends \Hypervel\Database\Eloquent\Relations\HasOneOrMany<TRelatedModel, TDeclaringModel, \Hypervel\Database\Eloquent\Collection<int, TRelatedModel>>
  */
-class HasMany extends BaseHasMany implements RelationContract
+class HasMany extends HasOneOrMany
 {
-    use WithoutAddConstraints;
-
     /**
-     * @template TValue
+     * Convert the relationship to a "has one" relationship.
      *
-     * @param array|(Closure(): TValue) $columns
-     * @param null|(Closure(): TValue) $callback
-     * @return TRelatedModel|TValue
+     * @return \Hypervel\Database\Eloquent\Relations\HasOne<TRelatedModel, TDeclaringModel>
      */
-    public function firstOr($columns = ['*'], ?Closure $callback = null)
+    public function one()
     {
-        if ($columns instanceof Closure) {
-            $callback = $columns;
-            $columns = ['*'];
+        return HasOne::noConstraints(fn () => tap(
+            new HasOne(
+                $this->getQuery(),
+                $this->parent,
+                $this->foreignKey,
+                $this->localKey
+            ),
+            function ($hasOne) {
+                if ($inverse = $this->getInverseRelationship()) {
+                    $hasOne->inverse($inverse);
+                }
+            }
+        ));
+    }
+
+    /** @inheritDoc */
+    public function getResults()
+    {
+        return ! is_null($this->getParentKey())
+            ? $this->query->get()
+            : $this->related->newCollection();
+    }
+
+    /** @inheritDoc */
+    public function initRelation(array $models, $relation)
+    {
+        foreach ($models as $model) {
+            $model->setRelation($relation, $this->related->newCollection());
         }
 
-        if (! is_null($model = $this->first($columns))) {
-            return $model;
-        }
+        return $models;
+    }
 
-        return $callback();
+    /** @inheritDoc */
+    public function match(array $models, EloquentCollection $results, $relation)
+    {
+        return $this->matchMany($models, $results, $relation);
     }
 }
