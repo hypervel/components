0ee5ff6b3dff2e6265783533a162317de9e21be4
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-01-20 10:47:42 +0000

    port HasManyThrough relation from Laravel

--- src/database/src/Eloquent/Relations/HasManyThrough.php
+++ src/database/src/Eloquent/Relations/HasManyThrough.php
@@ -4,31 +4,75 @@ declare(strict_types=1);
 
 namespace Hypervel\Database\Eloquent\Relations;
 
-use Hyperf\Database\Model\Relations\HasManyThrough as BaseHasManyThrough;
-use Hypervel\Database\Eloquent\Relations\Concerns\WithoutAddConstraints;
-use Hypervel\Database\Eloquent\Relations\Contracts\Relation as RelationContract;
+use Hypervel\Database\Eloquent\Builder;
+use Hypervel\Database\Eloquent\Collection as EloquentCollection;
+use Hypervel\Database\Eloquent\Relations\Concerns\InteractsWithDictionary;
 
 /**
  * @template TRelatedModel of \Hypervel\Database\Eloquent\Model
  * @template TIntermediateModel of \Hypervel\Database\Eloquent\Model
- * @template TParentModel of \Hypervel\Database\Eloquent\Model
+ * @template TDeclaringModel of \Hypervel\Database\Eloquent\Model
  *
- * @implements RelationContract<TRelatedModel, TParentModel, \Hypervel\Database\Eloquent\Collection<int, TRelatedModel>>
- *
- * @method \Hypervel\Database\Eloquent\Collection<int, TRelatedModel> get(array|string $columns = ['*'])
- * @method null|TRelatedModel first(array|string $columns = ['*'])
- * @method TRelatedModel firstOrFail(array|string $columns = ['*'])
- * @method \Hypervel\Database\Eloquent\Collection<int, TRelatedModel> findMany(mixed $ids, array|string $columns = ['*'])
- * @method TRelatedModel findOrFail(mixed $id, array|string $columns = ['*'])
- * @method null|TRelatedModel find(mixed $id, array|string $columns = ['*'])
- * @method \Hypervel\Database\Eloquent\Builder<TRelatedModel> getQuery()
- * @method \Hypervel\Database\Eloquent\Collection<int, TRelatedModel> chunk(int $count, callable $callback)
- * @method \Hypervel\Support\LazyCollection<int, TRelatedModel> lazy(int $chunkSize = 1000)
- * @method \Hypervel\Support\LazyCollection<int, TRelatedModel> lazyById(int $chunkSize = 1000, ?string $column = null, ?string $alias = null)
- * @method \Hypervel\Support\LazyCollection<int, TRelatedModel> lazyByIdDesc(int $chunkSize = 1000, ?string $column = null, ?string $alias = null)
- * @method \Hypervel\Database\Eloquent\Collection<int, TRelatedModel> getResults()
+ * @extends \Hypervel\Database\Eloquent\Relations\HasOneOrManyThrough<TRelatedModel, TIntermediateModel, TDeclaringModel, \Hypervel\Database\Eloquent\Collection<int, TRelatedModel>>
  */
-class HasManyThrough extends BaseHasManyThrough implements RelationContract
+class HasManyThrough extends HasOneOrManyThrough
 {
-    use WithoutAddConstraints;
+    use InteractsWithDictionary;
+
+    /**
+     * Convert the relationship to a "has one through" relationship.
+     *
+     * @return \Hypervel\Database\Eloquent\Relations\HasOneThrough<TRelatedModel, TIntermediateModel, TDeclaringModel>
+     */
+    public function one()
+    {
+        return HasOneThrough::noConstraints(fn () => new HasOneThrough(
+            tap($this->getQuery(), fn (Builder $query) => $query->getQuery()->joins = []),
+            $this->farParent,
+            $this->throughParent,
+            $this->getFirstKeyName(),
+            $this->getForeignKeyName(),
+            $this->getLocalKeyName(),
+            $this->getSecondLocalKeyName(),
+        ));
+    }
+
+    /** @inheritDoc */
+    public function initRelation(array $models, $relation)
+    {
+        foreach ($models as $model) {
+            $model->setRelation($relation, $this->related->newCollection());
+        }
+
+        return $models;
+    }
+
+    /** @inheritDoc */
+    public function match(array $models, EloquentCollection $results, $relation)
+    {
+        $dictionary = $this->buildDictionary($results);
+
+        // Once we have the dictionary we can simply spin through the parent models to
+        // link them up with their children using the keyed dictionary to make the
+        // matching very convenient and easy work. Then we'll just return them.
+        foreach ($models as $model) {
+            $key = $this->getDictionaryKey($model->getAttribute($this->localKey));
+
+            if ($key !== null && isset($dictionary[$key])) {
+                $model->setRelation(
+                    $relation, $this->related->newCollection($dictionary[$key])
+                );
+            }
+        }
+
+        return $models;
+    }
+
+    /** @inheritDoc */
+    public function getResults()
+    {
+        return ! is_null($this->farParent->{$this->localKey})
+            ? $this->get()
+            : $this->related->newCollection();
+    }
 }
