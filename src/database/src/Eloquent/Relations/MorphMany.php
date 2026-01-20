6dccaae65a012c98a4e9a362a83f1080dd420c7e
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-01-20 10:42:38 +0000

    port MorphMany relation from Laravel

--- src/database/src/Eloquent/Relations/MorphMany.php
+++ src/database/src/Eloquent/Relations/MorphMany.php
@@ -4,34 +4,68 @@ declare(strict_types=1);
 
 namespace Hypervel\Database\Eloquent\Relations;
 
-use Hyperf\Database\Model\Relations\MorphMany as BaseMorphMany;
-use Hypervel\Database\Eloquent\Relations\Concerns\WithoutAddConstraints;
-use Hypervel\Database\Eloquent\Relations\Contracts\Relation as RelationContract;
+use Hypervel\Database\Eloquent\Collection as EloquentCollection;
 
 /**
  * @template TRelatedModel of \Hypervel\Database\Eloquent\Model
- * @template TParentModel of \Hypervel\Database\Eloquent\Model
+ * @template TDeclaringModel of \Hypervel\Database\Eloquent\Model
  *
- * @implements RelationContract<TRelatedModel, TParentModel, \Hypervel\Database\Eloquent\Collection<int, TRelatedModel>>
- *
- * @method \Hypervel\Database\Eloquent\Collection<int, TRelatedModel> get(array|string $columns = ['*'])
- * @method TRelatedModel make(array $attributes = [])
- * @method TRelatedModel create(array $attributes = [])
- * @method TRelatedModel firstOrNew(array $attributes = [], array $values = [])
- * @method TRelatedModel firstOrCreate(array $attributes = [], array $values = [])
- * @method TRelatedModel updateOrCreate(array $attributes, array $values = [])
- * @method null|TRelatedModel first(array|string $columns = ['*'])
- * @method TRelatedModel firstOrFail(array|string $columns = ['*'])
- * @method \Hypervel\Database\Eloquent\Collection<int, TRelatedModel> findMany(mixed $ids, array|string $columns = ['*'])
- * @method TRelatedModel findOrFail(mixed $id, array|string $columns = ['*'])
- * @method null|TRelatedModel find(mixed $id, array|string $columns = ['*'])
- * @method \Hypervel\Database\Eloquent\Builder<TRelatedModel> getQuery()
- * @method \Hypervel\Support\LazyCollection<int, TRelatedModel> lazy(int $chunkSize = 1000)
- * @method \Hypervel\Support\LazyCollection<int, TRelatedModel> lazyById(int $chunkSize = 1000, ?string $column = null, ?string $alias = null)
- * @method \Hypervel\Support\LazyCollection<int, TRelatedModel> lazyByIdDesc(int $chunkSize = 1000, ?string $column = null, ?string $alias = null)
- * @method \Hypervel\Database\Eloquent\Collection<int, TRelatedModel> getResults()
+ * @extends \Hypervel\Database\Eloquent\Relations\MorphOneOrMany<TRelatedModel, TDeclaringModel, \Hypervel\Database\Eloquent\Collection<int, TRelatedModel>>
  */
-class MorphMany extends BaseMorphMany implements RelationContract
+class MorphMany extends MorphOneOrMany
 {
-    use WithoutAddConstraints;
+    /**
+     * Convert the relationship to a "morph one" relationship.
+     *
+     * @return \Hypervel\Database\Eloquent\Relations\MorphOne<TRelatedModel, TDeclaringModel>
+     */
+    public function one()
+    {
+        return MorphOne::noConstraints(fn () => tap(
+            new MorphOne(
+                $this->getQuery(),
+                $this->getParent(),
+                $this->morphType,
+                $this->foreignKey,
+                $this->localKey
+            ),
+            function ($morphOne) {
+                if ($inverse = $this->getInverseRelationship()) {
+                    $morphOne->inverse($inverse);
+                }
+            }
+        ));
+    }
+
+    /** @inheritDoc */
+    public function getResults()
+    {
+        return ! is_null($this->getParentKey())
+            ? $this->query->get()
+            : $this->related->newCollection();
+    }
+
+    /** @inheritDoc */
+    public function initRelation(array $models, $relation)
+    {
+        foreach ($models as $model) {
+            $model->setRelation($relation, $this->related->newCollection());
+        }
+
+        return $models;
+    }
+
+    /** @inheritDoc */
+    public function match(array $models, EloquentCollection $results, $relation)
+    {
+        return $this->matchMany($models, $results, $relation);
+    }
+
+    /** @inheritDoc */
+    public function forceCreate(array $attributes = [])
+    {
+        $attributes[$this->getMorphType()] = $this->morphClass;
+
+        return parent::forceCreate($attributes);
+    }
 }
