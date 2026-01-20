a66a111f6007c53b7479fe9fb2e11b11faca29ba
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-01-20 17:50:15 +0000

    Port BelongsToManyRelationship.php from Laravel
    Re-port with prependState() call for pendingAttributes support.

--- src/database/src/Eloquent/Factories/BelongsToManyRelationship.php
+++ src/database/src/Eloquent/Factories/BelongsToManyRelationship.php
@@ -4,43 +4,75 @@ declare(strict_types=1);
 
 namespace Hypervel\Database\Eloquent\Factories;
 
-use Hypervel\Database\Eloquent\Collection as EloquentCollection;
 use Hypervel\Database\Eloquent\Model;
 use Hypervel\Support\Collection;
 
 class BelongsToManyRelationship
 {
     /**
-     * Create a new attached relationship definition.
-     * @param $factory the related factory instance or model, or an array of models
-     * @param array|callable $pivot the pivot attributes / attribute resolver
-     * @param $relationship The relationship name
+     * The related factory instance.
+     *
+     * @var \Hypervel\Database\Eloquent\Factories\Factory|\Hypervel\Support\Collection|\Hypervel\Database\Eloquent\Model|array
      */
-    public function __construct(
-        protected array|EloquentCollection|Factory|Model $factory,
-        protected mixed $pivot,
-        protected string $relationship
-    ) {
+    protected $factory;
+
+    /**
+     * The pivot attributes / attribute resolver.
+     *
+     * @var callable|array
+     */
+    protected $pivot;
+
+    /**
+     * The relationship name.
+     *
+     * @var string
+     */
+    protected $relationship;
+
+    /**
+     * Create a new attached relationship definition.
+     *
+     * @param  \Hypervel\Database\Eloquent\Factories\Factory|\Hypervel\Support\Collection|\Hypervel\Database\Eloquent\Model|array  $factory
+     * @param  callable|array  $pivot
+     * @param  string  $relationship
+     */
+    public function __construct($factory, $pivot, $relationship)
+    {
+        $this->factory = $factory;
+        $this->pivot = $pivot;
+        $this->relationship = $relationship;
     }
 
     /**
      * Create the attached relationship for the given model.
+     *
+     * @param  \Hypervel\Database\Eloquent\Model  $model
+     * @return void
      */
-    public function createFor(Model $model): void
+    public function createFor(Model $model)
     {
-        Collection::wrap($this->factory instanceof Factory ? $this->factory->create([], $model) : $this->factory)
-            ->each(function ($attachable) use ($model) {
-                $model->{$this->relationship}()->attach(
-                    $attachable,
-                    is_callable($this->pivot) ? call_user_func($this->pivot, $model) : $this->pivot
-                );
-            });
+        $factoryInstance = $this->factory instanceof Factory;
+
+        if ($factoryInstance) {
+            $relationship = $model->{$this->relationship}();
+        }
+
+        Collection::wrap($factoryInstance ? $this->factory->prependState($relationship->getQuery()->pendingAttributes)->create([], $model) : $this->factory)->each(function ($attachable) use ($model) {
+            $model->{$this->relationship}()->attach(
+                $attachable,
+                is_callable($this->pivot) ? call_user_func($this->pivot, $model) : $this->pivot
+            );
+        });
     }
 
     /**
      * Specify the model instances to always use when creating relationships.
+     *
+     * @param  \Hypervel\Support\Collection  $recycle
+     * @return $this
      */
-    public function recycle(Collection $recycle): self
+    public function recycle($recycle)
     {
         if ($this->factory instanceof Factory) {
             $this->factory = $this->factory->recycle($recycle);
