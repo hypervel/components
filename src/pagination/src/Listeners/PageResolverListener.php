bff8809f31413d8e9028c7cd27f5f3b7c7ba4adf
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-01-22 08:03:22 +0000

    Refactor PageResolverListener to use constructor injection
    Follow Hypervel pattern: inject ContainerInterface via constructor
    instead of using ApplicationContext::getContainer() inline.

--- src/pagination/src/Listeners/PageResolverListener.php
+++ src/pagination/src/Listeners/PageResolverListener.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Hypervel\Pagination\Listeners;
 
-use Hyperf\Context\ApplicationContext;
 use Hyperf\Context\Context;
 use Hyperf\Event\Contract\ListenerInterface;
 use Hyperf\Framework\Event\BootApplication;
@@ -12,10 +11,16 @@ use Hyperf\HttpServer\Contract\RequestInterface;
 use Hypervel\Pagination\Cursor;
 use Hypervel\Pagination\CursorPaginator;
 use Hypervel\Pagination\Paginator;
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 class PageResolverListener implements ListenerInterface
 {
+    public function __construct(
+        protected ContainerInterface $container
+    ) {
+    }
+
     /**
      * @return array<int, class-string>
      */
@@ -32,15 +37,13 @@ class PageResolverListener implements ListenerInterface
      */
     public function process(object $event): void
     {
-        Paginator::currentPageResolver(function (string $pageName = 'page'): int {
-            if (! ApplicationContext::hasContainer()
-                || ! interface_exists(RequestInterface::class)
-                || ! Context::has(ServerRequestInterface::class)
-            ) {
+        $container = $this->container;
+
+        Paginator::currentPageResolver(function (string $pageName = 'page') use ($container): int {
+            if (! Context::has(ServerRequestInterface::class)) {
                 return 1;
             }
 
-            $container = ApplicationContext::getContainer();
             $page = $container->get(RequestInterface::class)->input($pageName);
 
             if (filter_var($page, FILTER_VALIDATE_INT) !== false && (int) $page >= 1) {
@@ -50,40 +53,27 @@ class PageResolverListener implements ListenerInterface
             return 1;
         });
 
-        Paginator::currentPathResolver(function (): string {
-            $default = '/';
-            if (! ApplicationContext::hasContainer()
-                || ! interface_exists(RequestInterface::class)
-                || ! Context::has(ServerRequestInterface::class)
-            ) {
-                return $default;
+        Paginator::currentPathResolver(function () use ($container): string {
+            if (! Context::has(ServerRequestInterface::class)) {
+                return '/';
             }
 
-            $container = ApplicationContext::getContainer();
             return $container->get(RequestInterface::class)->url();
         });
 
-        Paginator::queryStringResolver(function (): array {
-            if (! ApplicationContext::hasContainer()
-                || ! interface_exists(RequestInterface::class)
-                || ! Context::has(ServerRequestInterface::class)
-            ) {
+        Paginator::queryStringResolver(function () use ($container): array {
+            if (! Context::has(ServerRequestInterface::class)) {
                 return [];
             }
 
-            $container = ApplicationContext::getContainer();
             return $container->get(RequestInterface::class)->getQueryParams();
         });
 
-        CursorPaginator::currentCursorResolver(function (string $cursorName = 'cursor'): ?Cursor {
-            if (! ApplicationContext::hasContainer()
-                || ! interface_exists(RequestInterface::class)
-                || ! Context::has(ServerRequestInterface::class)
-            ) {
+        CursorPaginator::currentCursorResolver(function (string $cursorName = 'cursor') use ($container): ?Cursor {
+            if (! Context::has(ServerRequestInterface::class)) {
                 return null;
             }
 
-            $container = ApplicationContext::getContainer();
             return Cursor::fromEncoded($container->get(RequestInterface::class)->input($cursorName));
         });
     }
