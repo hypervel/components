33c85272dadc7b899959b7bab7cbb4884599a7ce
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-01-22 07:54:44 +0000

    Add pagination ConfigProvider
    Basic ConfigProvider for the pagination package. Pagination state
    resolvers are configured via PaginationState::resolveUsing().

---
+++ src/pagination/src/ConfigProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Pagination;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [],
+            'publish' => [],
+        ];
+    }
+}
