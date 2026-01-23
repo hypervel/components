901e799d1d095fffc72bd25d4b6f270b9907e904
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-01-23 06:04:38 +0000

    Remove redundant is_string checks in TransformsToResourceCollection
    guessResourceName() returns array<int, class-string<...>> - elements
    are always strings. String concatenation also always produces strings.
    The is_string() checks before class_exists() were dead code.

--- src/collections/src/Traits/TransformsToResourceCollection.php
+++ src/collections/src/Traits/TransformsToResourceCollection.php
@@ -69,13 +69,13 @@ trait TransformsToResourceCollection
         foreach ($resourceClasses as $resourceClass) {
             $resourceCollection = $resourceClass.'Collection';
 
-            if (is_string($resourceCollection) && class_exists($resourceCollection)) {
+            if (class_exists($resourceCollection)) {
                 return new $resourceCollection($this);
             }
         }
 
         foreach ($resourceClasses as $resourceClass) {
-            if (is_string($resourceClass) && class_exists($resourceClass)) {
+            if (class_exists($resourceClass)) {
                 return $resourceClass::collection($this);
             }
         }
