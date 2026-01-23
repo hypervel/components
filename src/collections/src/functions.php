0e88c563f0c705d801b55fde03321df85d198564
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-01-23 02:20:04 +0000

    Add functions.php and helpers.php from Laravel
    - functions.php: enum_value() helper function
    - helpers.php: collect(), data_fill(), data_get(), data_set(),
      data_forget(), data_has(), head(), last(), value(), when()
    
    All updated with Hypervel namespaces.

---
+++ src/collections/src/functions.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Support;
+
+if (! function_exists('Hypervel\Support\enum_value')) {
+    /**
+     * Return a scalar value for the given value that might be an enum.
+     *
+     * @internal
+     *
+     * @template TValue
+     * @template TDefault
+     *
+     * @param  TValue  $value
+     * @param  TDefault|callable(TValue): TDefault  $default
+     * @return ($value is empty ? TDefault : mixed)
+     */
+    function enum_value($value, $default = null)
+    {
+        return match (true) {
+            $value instanceof \BackedEnum => $value->value,
+            $value instanceof \UnitEnum => $value->name,
+
+            default => $value ?? value($default),
+        };
+    }
+}
