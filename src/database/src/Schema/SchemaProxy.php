c8642d74ad88f739ef942ed21ee65a85e0929dd9
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-01-25 12:23:41 +0000

    Use Hypervel\Context\ApplicationContext in SchemaProxy

--- src/database/src/Schema/SchemaProxy.php
+++ src/database/src/Schema/SchemaProxy.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Hypervel\Database\Schema;
 
-use Hyperf\Context\ApplicationContext;
+use Hypervel\Context\ApplicationContext;
 use Hypervel\Database\DatabaseManager;
 
 /**
