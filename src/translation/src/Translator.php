704e97a28fb261759db8b1e9219aa49dc8927f62
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-01-25 06:25:20 +0000

    Add enum support to Translator replacements
    Translator now automatically converts enum values in replacement
    arrays using enum_value() - BackedEnum uses value, UnitEnum uses name.

--- src/translation/src/Translator.php
+++ src/translation/src/Translator.php
@@ -16,6 +16,8 @@ use Hypervel\Contracts\Translation\Loader;
 use Hypervel\Contracts\Translation\Translator as TranslatorContract;
 use InvalidArgumentException;
 
+use function Hypervel\Support\enum_value;
+
 class Translator extends NamespacedItemResolver implements TranslatorContract
 {
     use Macroable;
@@ -243,8 +245,10 @@ class Translator extends NamespacedItemResolver implements TranslatorContract
                 continue;
             }
 
-            if (is_object($value) && isset($this->stringableHandlers[get_class($value)])) {
-                $value = call_user_func($this->stringableHandlers[get_class($value)], $value);
+            if (is_object($value)) {
+                $value = isset($this->stringableHandlers[get_class($value)])
+                    ? call_user_func($this->stringableHandlers[get_class($value)], $value)
+                    : enum_value($value);
             }
 
             $key = (string) $key;
