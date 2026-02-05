ae05bb8f14d7a9925a108c86b02d28acc16b5325
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-05 09:04:34 +0000

    Add tests for afterCreated callback functionality

--- tests/Coroutine/CoroutineTest.php
+++ tests/Coroutine/CoroutineTest.php
@@ -79,4 +79,92 @@ class CoroutineTest extends CoroutineTestCase
 
         $this->assertTrue(true);
     }
+
+    public function testAfterCreatedCallbacksAreExecuted(): void
+    {
+        $executed = false;
+
+        Coroutine::afterCreated(function () use (&$executed) {
+            $executed = true;
+        });
+
+        Coroutine::create(function () {
+            // The afterCreated callback should have run before this
+        });
+
+        $this->assertTrue($executed);
+
+        // Clean up
+        Coroutine::flushAfterCreated();
+    }
+
+    public function testAfterCreatedCallbacksExecuteInOrder(): void
+    {
+        $order = [];
+
+        Coroutine::afterCreated(function () use (&$order) {
+            $order[] = 1;
+        });
+
+        Coroutine::afterCreated(function () use (&$order) {
+            $order[] = 2;
+        });
+
+        Coroutine::create(function () use (&$order) {
+            $order[] = 3;
+        });
+
+        $this->assertSame([1, 2, 3], $order);
+
+        // Clean up
+        Coroutine::flushAfterCreated();
+    }
+
+    public function testFlushAfterCreatedClearsCallbacks(): void
+    {
+        $count = 0;
+
+        Coroutine::afterCreated(function () use (&$count) {
+            $count++;
+        });
+
+        Coroutine::create(function () {});
+        $this->assertSame(1, $count);
+
+        Coroutine::flushAfterCreated();
+
+        Coroutine::create(function () {});
+        $this->assertSame(1, $count); // Should still be 1, callback was flushed
+    }
+
+    public function testAfterCreatedCallbackExceptionDoesNotStopOthers(): void
+    {
+        $container = Mockery::mock(ContainerContract::class);
+        ApplicationContext::setContainer($container);
+        $container->shouldReceive('has')->with(ExceptionHandlerContract::class)->andReturnTrue();
+        $container->shouldReceive('get')->with(ExceptionHandlerContract::class)
+            ->andReturn($handler = Mockery::mock(ExceptionHandlerContract::class));
+        $handler->shouldReceive('report')->once();
+
+        $secondCallbackRan = false;
+        $mainCallableRan = false;
+
+        Coroutine::afterCreated(function () {
+            throw new Exception('First callback fails');
+        });
+
+        Coroutine::afterCreated(function () use (&$secondCallbackRan) {
+            $secondCallbackRan = true;
+        });
+
+        Coroutine::create(function () use (&$mainCallableRan) {
+            $mainCallableRan = true;
+        });
+
+        $this->assertTrue($secondCallbackRan);
+        $this->assertTrue($mainCallableRan);
+
+        // Clean up
+        Coroutine::flushAfterCreated();
+    }
 }
