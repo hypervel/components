650bb85c1da6723f92918f6a875d7ea9182e8302
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-05 07:32:37 +0000

    Port Locker.php from Hyperf to Hypervel
    Update namespace and imports to use Hypervel equivalents.

--- src/coroutine/src/Locker.php
+++ src/coroutine/src/Locker.php
@@ -4,8 +4,42 @@ declare(strict_types=1);
 
 namespace Hypervel\Coroutine;
 
-use Hyperf\Coroutine\Locker as BaseLocker;
+use Hypervel\Engine\Channel;
 
-class Locker extends BaseLocker
+class Locker
 {
+    /**
+     * @var array<string, Channel|null>
+     */
+    protected static array $channels = [];
+
+    /**
+     * Acquire a lock for the given key.
+     *
+     * Returns true if this is the first lock acquisition (owner),
+     * or false if waiting on an existing lock.
+     */
+    public static function lock(string $key): bool
+    {
+        if (! isset(static::$channels[$key])) {
+            static::$channels[$key] = new Channel(1);
+            return true;
+        }
+
+        $channel = static::$channels[$key];
+        $channel->pop(-1);
+        return false;
+    }
+
+    /**
+     * Release the lock for the given key.
+     */
+    public static function unlock(string $key): void
+    {
+        if (isset(static::$channels[$key])) {
+            $channel = static::$channels[$key];
+            static::$channels[$key] = null;
+            $channel->close();
+        }
+    }
 }
