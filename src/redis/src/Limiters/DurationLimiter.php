06c5306826018976aad1954794027712c69e0493
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-07 03:32:54 +0000

    refactor(redis): merge DurationLimiter to standalone Redis types

--- src/redis/src/Limiters/DurationLimiter.php
+++ src/redis/src/Limiters/DurationLimiter.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Hypervel\Redis\Limiters;
 
-use Hyperf\Redis\RedisFactory;
-use Hyperf\Redis\RedisProxy;
+use Hypervel\Redis\RedisFactory;
+use Hypervel\Redis\RedisProxy;
 
 class DurationLimiter
 {
