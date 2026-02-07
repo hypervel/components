9061f048b12fa55606dd267fb0c691f38be925b4
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-07 03:21:02 +0000

    feat(redis): add standalone redis Pool\RedisPool implementation

---
+++ src/redis/src/Pool/RedisPool.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Redis\Pool;
+
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\Contract\ConnectionInterface;
+use Hypervel\Pool\Pool;
+use Hypervel\Redis\Frequency;
+use Hypervel\Redis\RedisConnection;
+use Hypervel\Support\Arr;
+use InvalidArgumentException;
+use Psr\Container\ContainerInterface;
+
+class RedisPool extends Pool
+{
+    protected array $config;
+
+    /**
+     * Create a new Redis pool instance.
+     */
+    public function __construct(
+        ContainerInterface $container,
+        protected string $name
+    ) {
+        $configService = $container->get(ConfigInterface::class);
+        $key = sprintf('redis.%s', $this->name);
+
+        if (! $configService->has($key)) {
+            throw new InvalidArgumentException(sprintf('config[%s] is not exist!', $key));
+        }
+
+        $this->config = $configService->get($key);
+        $poolOptions = Arr::get($this->config, 'pool', []);
+
+        $this->frequency = new Frequency($this);
+
+        parent::__construct($container, $poolOptions);
+    }
+
+    /**
+     * Get the pool name.
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the Redis connection configuration.
+     */
+    public function getConfig(): array
+    {
+        return $this->config;
+    }
+
+    /**
+     * Create a new pooled Redis connection.
+     */
+    protected function createConnection(): ConnectionInterface
+    {
+        return new RedisConnection($this->container, $this, $this->config);
+    }
+}
