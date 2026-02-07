35f778a09813a45a64e5e79e41f7df2fd582835f
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-07 03:30:20 +0000

    refactor(redis): adapt legacy RedisPool wrapper to standalone Pool\RedisPool

--- src/redis/src/RedisPool.php
+++ src/redis/src/RedisPool.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 namespace Hypervel\Redis;
 
-use Hyperf\Contract\ConnectionInterface;
-use Hyperf\Redis\Pool\RedisPool as HyperfRedisPool;
+use Hypervel\Redis\Pool\RedisPool as StandaloneRedisPool;
 
-class RedisPool extends HyperfRedisPool
+/**
+ * @deprecated Use \Hypervel\Redis\Pool\RedisPool instead.
+ */
+class RedisPool extends StandaloneRedisPool
 {
-    protected function createConnection(): ConnectionInterface
-    {
-        return new RedisConnection($this->container, $this, $this->config);
-    }
 }
