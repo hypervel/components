86b1244d2a21b16d34733d4f0e47f2cb4c1d25d9
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-08 15:39:29 +0000

    Switch redis package config to concrete class for typed property

--- src/redis/src/RedisConfig.php
+++ src/redis/src/RedisConfig.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Hypervel\Redis;
 
-use Hypervel\Contracts\Config\Repository;
+use Hypervel\Config\Repository;
 use InvalidArgumentException;
 
 class RedisConfig
