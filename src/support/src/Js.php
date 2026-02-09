a72a6c69def919676c961270dfda0035c742d457
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-09 08:43:26 +0000

    support: align js helper behavior with laravel parity

--- src/support/src/Js.php
+++ src/support/src/Js.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Hypervel\Support;
 
-use Hyperf\ViewEngine\Contract\Htmlable;
 use Hypervel\Contracts\Support\Arrayable;
+use Hypervel\Contracts\Support\Htmlable;
 use Hypervel\Contracts\Support\Jsonable;
 use JsonException;
 use JsonSerializable;
@@ -24,7 +24,12 @@ class Js implements Htmlable, Stringable
      *
      * @var int
      */
-    protected const REQUIRED_FLAGS = JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT | JSON_THROW_ON_ERROR;
+    protected const REQUIRED_FLAGS = JSON_HEX_TAG
+        | JSON_HEX_APOS
+        | JSON_HEX_AMP
+        | JSON_HEX_QUOT
+        | JSON_UNESCAPED_UNICODE
+        | JSON_THROW_ON_ERROR;
 
     /**
      * Create a new class instance.
@@ -57,6 +62,13 @@ class Js implements Htmlable, Stringable
             return $data->toHtml();
         }
 
+        if ($data instanceof Htmlable
+            && ! $data instanceof Arrayable
+            && ! $data instanceof Jsonable
+            && ! $data instanceof JsonSerializable) {
+            $data = $data->toHtml();
+        }
+
         if ($data instanceof UnitEnum) {
             $data = enum_value($data);
         }
@@ -78,7 +90,7 @@ class Js implements Htmlable, Stringable
     public static function encode(mixed $data, int $flags = 0, int $depth = 512): string
     {
         if ($data instanceof Jsonable) {
-            return (string) $data;
+            return $data->toJson($flags | static::REQUIRED_FLAGS);
         }
 
         if ($data instanceof Arrayable && ! ($data instanceof JsonSerializable)) {
