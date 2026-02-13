0ba8bf83eabadd925767dfd4755ab192db3b6c14
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-13 04:10:38 +0000

    Replace ApplicationContext with Container::getInstance() in broadcasting package

--- src/broadcasting/src/UniqueBroadcastEvent.php
+++ src/broadcasting/src/UniqueBroadcastEvent.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Hypervel\Broadcasting;
 
-use Hypervel\Context\ApplicationContext;
+use Hypervel\Container\Container;
 use Hypervel\Contracts\Cache\Factory as Cache;
 use Hypervel\Contracts\Queue\ShouldBeUnique;
 
@@ -49,6 +49,6 @@ class UniqueBroadcastEvent extends BroadcastEvent implements ShouldBeUnique
     {
         return method_exists($this->event, 'uniqueVia')
             ? $this->event->uniqueVia()
-            : ApplicationContext::getContainer()->get(Cache::class);
+            : Container::getInstance()->make(Cache::class);
     }
 }
