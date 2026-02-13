380032caeea6a5b50b48340fe9c2420eb892c525
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-13 05:55:16 +0000

    Replace ApplicationContext with Container::getInstance() in translation package

--- src/translation/src/Functions.php
+++ src/translation/src/Functions.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Hypervel\Translation;
 
 use Countable;
-use Hypervel\Context\ApplicationContext;
+use Hypervel\Container\Container;
 use Hypervel\Contracts\Translation\Translator as TranslatorContract;
 
 /**
@@ -13,8 +13,8 @@ use Hypervel\Contracts\Translation\Translator as TranslatorContract;
  */
 function __(?string $key = null, array $replace = [], ?string $locale = null): array|string
 {
-    return ApplicationContext::getContainer()
-        ->get(TranslatorContract::class)
+    return Container::getInstance()
+        ->make(TranslatorContract::class)
         ->trans($key, $replace, $locale);
 }
 
@@ -26,12 +26,12 @@ function __(?string $key = null, array $replace = [], ?string $locale = null): a
 function trans(?string $key = null, array $replace = [], ?string $locale = null): array|string|TranslatorContract
 {
     if (is_null($key)) {
-        ApplicationContext::getContainer()
-            ->get(TranslatorContract::class);
+        Container::getInstance()
+            ->make(TranslatorContract::class);
     }
 
-    return ApplicationContext::getContainer()
-        ->get(TranslatorContract::class)
+    return Container::getInstance()
+        ->make(TranslatorContract::class)
         ->get($key, $replace, $locale);
 }
 
@@ -40,7 +40,7 @@ function trans(?string $key = null, array $replace = [], ?string $locale = null)
  */
 function trans_choice(string $key, array|Countable|float|int $number, array $replace = [], ?string $locale = null): string
 {
-    return ApplicationContext::getContainer()
-        ->get(TranslatorContract::class)
+    return Container::getInstance()
+        ->make(TranslatorContract::class)
         ->transChoice($key, $number, $replace, $locale);
 }
