966e703ff5022aac960abd02023de8d96a16f8b9
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-14 15:33:36 +0000

    Port OnPipeMessageListener from Hyperf websocket-server

---
+++ src/websocket-server/src/Listener/OnPipeMessageListener.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\WebSocketServer\Listener;
+
+use Hyperf\Contract\StdoutLoggerInterface;
+use Hypervel\Contracts\Container\Container;
+use Hypervel\Event\Contracts\ListenerInterface;
+use Hypervel\ExceptionHandler\Formatter\FormatterInterface;
+use Hypervel\Framework\Events\OnPipeMessage;
+use Hypervel\WebSocketServer\Sender;
+use Hypervel\WebSocketServer\SenderPipeMessage;
+use Throwable;
+
+class OnPipeMessageListener implements ListenerInterface
+{
+    public function __construct(private Container $container, private StdoutLoggerInterface $logger, private Sender $sender)
+    {
+    }
+
+    /**
+     * @return string[] returns the events that you want to listen
+     */
+    public function listen(): array
+    {
+        return [
+            OnPipeMessage::class,
+        ];
+    }
+
+    /**
+     * Handle the Event when the event is triggered, all listeners will
+     * complete before the event is returned to the EventDispatcher.
+     */
+    public function process(object $event): void
+    {
+        if ($event instanceof OnPipeMessage && $event->data instanceof SenderPipeMessage) {
+            /** @var SenderPipeMessage $message */
+            $message = $event->data;
+
+            try {
+                [$fd, $method] = $this->sender->getFdAndMethodFromProxyMethod($message->name, $message->arguments);
+                $this->sender->proxy($fd, $method, $message->arguments);
+            } catch (Throwable $exception) {
+                $formatter = $this->container->get(FormatterInterface::class);
+                $this->logger->warning($formatter->format($exception));
+            }
+        }
+    }
+}
