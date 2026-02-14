a680bc713dba690e56e730ce7ae1faa3e71db4ff
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-14 15:32:26 +0000

    Port InvalidMethodException from Hyperf websocket-server

---
+++ src/websocket-server/src/Exception/InvalidMethodException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\WebSocketServer\Exception;
+
+use Hypervel\Server\Exceptions\ServerException;
+
+class InvalidMethodException extends ServerException
+{
+}
