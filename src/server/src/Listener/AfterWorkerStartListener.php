99c59c409c4502c89c33d4f4138101f2f400b3b5
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-14 15:26:43 +0000

    Add AfterWorkerStartListener to server package

---
+++ src/server/src/Listener/AfterWorkerStartListener.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Server\Listener;
+
+use Hyperf\Contract\StdoutLoggerInterface;
+use Hypervel\Engine\Constant\SocketType;
+use Hypervel\Event\Contracts\ListenerInterface;
+use Hypervel\Framework\Events\AfterWorkerStart;
+use Hypervel\Server\ServerInterface;
+use Hypervel\Server\ServerManager;
+use Psr\Log\LoggerInterface;
+use Swoole\Server\Port;
+
+class AfterWorkerStartListener implements ListenerInterface
+{
+    private LoggerInterface $logger;
+
+    public function __construct(StdoutLoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Get the events the listener should handle.
+     */
+    public function listen(): array
+    {
+        return [
+            AfterWorkerStart::class,
+        ];
+    }
+
+    /**
+     * Log server listening information after the first worker starts.
+     */
+    public function process(object $event): void
+    {
+        /** @var AfterWorkerStart $event */
+        if ($event->workerId === 0) {
+            /** @var Port $server */
+            foreach (ServerManager::list() as [$type, $server]) {
+                $listen = $server->host . ':' . $server->port;
+                $type = match ($type) {
+                    ServerInterface::SERVER_BASE => $this->resolveBaseServerType($server->type),
+                    ServerInterface::SERVER_WEBSOCKET => 'WebSocket',
+                    default => 'HTTP',
+                };
+                $this->logger->info(sprintf('%s Server listening at %s', $type, $listen));
+            }
+        }
+    }
+
+    /**
+     * Resolve the display type for a base server by its socket type.
+     */
+    private function resolveBaseServerType(int $sockType): string
+    {
+        if (in_array($sockType, [SocketType::TCP, SocketType::TCP6])) {
+            return 'TCP';
+        }
+        if (in_array($sockType, [SocketType::UDP, SocketType::UDP6])) {
+            return 'UDP';
+        }
+
+        return 'UNKNOWN';
+    }
+}
