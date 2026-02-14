8c3ccb64674b6c9cf81f752f0af675fe50161451
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-14 15:29:49 +0000

    Port EncodingException from Hyperf http-server

---
+++ src/http-server/src/Exceptions/Http/EncodingException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\HttpServer\Exceptions\Http;
+
+use Hypervel\Server\Exceptions\ServerException;
+
+class EncodingException extends ServerException
+{
+}
