ee97b0757de15de9cc58be6fd2817623357d9f3c
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-14 15:33:31 +0000

    Port InitSenderListener from Hyperf websocket-server

---
+++ src/websocket-server/src/Listener/InitSenderListener.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\WebSocketServer\Listener;
+
+use Hypervel\Contracts\Container\Container;
+use Hypervel\Event\Contracts\ListenerInterface;
+use Hypervel\Framework\Events\AfterWorkerStart;
+use Hypervel\WebSocketServer\Sender;
+
+class InitSenderListener implements ListenerInterface
+{
+    public function __construct(private Container $container)
+    {
+    }
+
+    /**
+     * @return string[] returns the events that you want to listen
+     */
+    public function listen(): array
+    {
+        return [
+            AfterWorkerStart::class,
+        ];
+    }
+
+    public function process(object $event): void
+    {
+        if ($this->container->has(Sender::class)) {
+            $sender = $this->container->get(Sender::class);
+            $sender->setWorkerId($event->workerId);
+        }
+    }
+}
