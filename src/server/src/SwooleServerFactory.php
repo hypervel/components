52c4e00e2cad11478f92766ec6ffc942335af645
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-14 15:26:28 +0000

    Add SwooleServerFactory to server package

---
+++ src/server/src/SwooleServerFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Server;
+
+use Psr\Container\ContainerInterface;
+use Swoole\Server as SwooleServer;
+
+class SwooleServerFactory
+{
+    /**
+     * Create the underlying Swoole server instance.
+     */
+    public function __invoke(ContainerInterface $container): SwooleServer
+    {
+        $factory = $container->get(ServerFactory::class);
+
+        return $factory->getServer()->getServer();
+    }
+}
