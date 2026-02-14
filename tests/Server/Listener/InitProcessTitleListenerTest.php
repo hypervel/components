5a740473434588848a1865d380c7a26e96c19957
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-14 15:27:35 +0000

    Port InitProcessTitleListenerTest from Hyperf server package

---
+++ tests/Server/Listener/InitProcessTitleListenerTest.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Tests\Server\Listener;
+
+use Hyperf\Config\Config;
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\Process\Event\BeforeProcessHandle;
+use Hypervel\Context\Context;
+use Hypervel\Framework\Events\AfterWorkerStart;
+use Hypervel\Framework\Events\OnManagerStart;
+use Hypervel\Framework\Events\OnStart;
+use Hypervel\Server\Listener\InitProcessTitleListener;
+use Hypervel\Tests\Server\Stub\DemoProcess;
+use Hypervel\Tests\Server\Stub\InitProcessTitleListenerStub;
+use Hypervel\Tests\Server\Stub\InitProcessTitleListenerStub2;
+use Hypervel\Tests\TestCase;
+use Mockery as m;
+use Psr\Container\ContainerInterface;
+use Psr\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class InitProcessTitleListenerTest extends TestCase
+{
+    public function testInitProcessTitleListenerListen()
+    {
+        $container = m::mock(ContainerInterface::class);
+        $container->shouldReceive('has')->with(m::any())->andReturn(false);
+
+        $listener = new InitProcessTitleListener($container);
+
+        $this->assertSame([
+            OnStart::class,
+            OnManagerStart::class,
+            AfterWorkerStart::class,
+            BeforeProcessHandle::class,
+        ], $listener->listen());
+    }
+
+    public function testProcessDefaultName()
+    {
+        $container = m::mock(ContainerInterface::class);
+        $container->shouldReceive('has')->with(m::any())->andReturn(false);
+
+        $listener = new InitProcessTitleListenerStub($container);
+        $process = new DemoProcess($container);
+
+        $listener->process(new BeforeProcessHandle($process, 1));
+
+        if (! $listener->isSupportedOS()) {
+            $this->assertSame(null, Context::get('test.server.process.title'));
+        } else {
+            $this->assertSame('test.demo.1', Context::get('test.server.process.title'));
+        }
+    }
+
+    public function testProcessName()
+    {
+        $name = 'hyperf-skeleton.' . uniqid();
+        $container = m::mock(ContainerInterface::class);
+        $container->shouldReceive('has')->with(ConfigInterface::class)->andReturn(true);
+        $container->shouldReceive('has')->with(EventDispatcherInterface::class)->andReturn(false);
+        $container->shouldReceive('get')->with(ConfigInterface::class)->andReturn(new Config([
+            'app_name' => $name,
+        ]));
+
+        $listener = new InitProcessTitleListenerStub($container);
+        $process = new DemoProcess($container);
+
+        $listener->process(new BeforeProcessHandle($process, 0));
+
+        if (! $listener->isSupportedOS()) {
+            $this->assertSame(null, Context::get('test.server.process.title'));
+        } else {
+            $this->assertSame($name . '.test.demo.0', Context::get('test.server.process.title'));
+        }
+    }
+
+    public function testUserDefinedDot()
+    {
+        $name = 'hyperf-skeleton.' . uniqid();
+        $container = m::mock(ContainerInterface::class);
+        $container->shouldReceive('has')->with(ConfigInterface::class)->andReturn(true);
+        $container->shouldReceive('has')->with(EventDispatcherInterface::class)->andReturn(false);
+        $container->shouldReceive('get')->with(ConfigInterface::class)->andReturn(new Config([
+            'app_name' => $name,
+        ]));
+
+        $listener = new InitProcessTitleListenerStub2($container);
+        $process = new DemoProcess($container);
+
+        $listener->process(new BeforeProcessHandle($process, 0));
+
+        if (! $listener->isSupportedOS()) {
+            $this->assertSame(null, Context::get('test.server.process.title'));
+        } else {
+            $this->assertSame($name . '#test.demo#0', Context::get('test.server.process.title'));
+        }
+    }
+}
