e5b6d2e97efb52858aa7d55826a215f6fc697f7c
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-14 15:44:22 +0000

    Port AbstractDispatcher base class implementing DispatcherInterface

---
+++ src/dispatcher/src/AbstractDispatcher.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Dispatcher;
+
+use Hyperf\Contract\DispatcherInterface;
+
+abstract class AbstractDispatcher implements DispatcherInterface
+{
+}
