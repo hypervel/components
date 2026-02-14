7e19166156fa124e4bcbfd3b29d914a89283fa85
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-14 15:40:02 +0000

    Port ExceptionHandlerListenerTest verifying config-based handler registration

---
+++ tests/ExceptionHandler/ExceptionHandlerListenerTest.php
@@ -0,0 +1,110 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Tests\ExceptionHandler;
+
+use Hyperf\Config\Config;
+use Hyperf\Di\Annotation\AnnotationCollector;
+use Hypervel\ExceptionHandler\Annotation\ExceptionHandler;
+use Hypervel\ExceptionHandler\Listener\ExceptionHandlerListener;
+use Hypervel\Tests\TestCase;
+use stdClass;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class ExceptionHandlerListenerTest extends TestCase
+{
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        AnnotationCollector::clear();
+    }
+
+    public function testConfig()
+    {
+        $config = new Config([
+            'exceptions' => [
+                'handler' => [
+                    'http' => $http = [
+                        'Foo', 'Bar',
+                    ],
+                    'ws' => $ws = [
+                        'Foo', 'Tar', 'Bar',
+                    ],
+                ],
+            ],
+        ]);
+        $listener = new ExceptionHandlerListener($config);
+        $listener->process(new stdClass());
+        $this->assertSame($http, $config->get('exceptions.handler', [])['http']);
+        $this->assertSame($ws, $config->get('exceptions.handler', [])['ws']);
+    }
+
+    public function testAnnotation()
+    {
+        $config = new Config([
+            'exceptions' => [
+                'handler' => [
+                    'http' => [
+                        'Foo', 'Bar',
+                    ],
+                ],
+            ],
+        ]);
+        AnnotationCollector::collectClass('Bar1', ExceptionHandler::class, new ExceptionHandler('http', 1));
+        $listener = new ExceptionHandlerListener($config);
+        $listener->process(new stdClass());
+        $this->assertSame([
+            'http' => [
+                'Bar1', 'Foo', 'Bar',
+            ],
+        ], $config->get('exceptions.handler', []));
+    }
+
+    public function testAnnotationWithSamePriotity()
+    {
+        $config = new Config([
+            'exceptions' => [
+                'handler' => [
+                    'http' => [
+                        'Foo', 'Bar',
+                    ],
+                    'ws' => [
+                        'Foo',
+                    ],
+                ],
+            ],
+        ]);
+        AnnotationCollector::collectClass('Bar1', ExceptionHandler::class, new ExceptionHandler('http', 0));
+        AnnotationCollector::collectClass('Bar', ExceptionHandler::class, new ExceptionHandler('ws', 1));
+        $listener = new ExceptionHandlerListener($config);
+        $listener->process(new stdClass());
+        $this->assertEquals(['Foo', 'Bar', 'Bar1'], $config->get('exceptions.handler', [])['http']);
+        $this->assertEquals(['Bar', 'Foo'], $config->get('exceptions.handler', [])['ws']);
+    }
+
+    public function testTheSameHandler()
+    {
+        $config = new Config([
+            'exceptions' => [
+                'handler' => [
+                    'http' => [
+                        'Foo', 'Bar', 'Bar', 'Tar',
+                    ],
+                ],
+            ],
+        ]);
+        AnnotationCollector::collectClass('Tar', ExceptionHandler::class, new ExceptionHandler('http', 1));
+        $listener = new ExceptionHandlerListener($config);
+        $listener->process(new stdClass());
+        $this->assertSame([
+            'http' => [
+                'Tar', 'Foo', 'Bar',
+            ],
+        ], $config->get('exceptions.handler', []));
+    }
+}
