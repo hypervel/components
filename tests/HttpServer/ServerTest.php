ce09a7a60dbac97121c6de31264a4f468d983b55
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-14 15:32:05 +0000

    Port ServerTest from Hyperf http-server

---
+++ tests/HttpServer/ServerTest.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Tests\HttpServer;
+
+use Hyperf\HttpMessage\Exception\BadRequestHttpException;
+use Hyperf\HttpMessage\Exception\HttpException;
+use Hyperf\HttpMessage\Server\Response as Psr7Response;
+use Hypervel\Context\ApplicationContext;
+use Hypervel\Contracts\Container\Container as ContainerContract;
+use Hypervel\Contracts\Debug\ExceptionHandler as ExceptionHandlerContract;
+use Hypervel\Coordinator\Constants;
+use Hypervel\Coordinator\CoordinatorManager;
+use Hypervel\Dispatcher\HttpDispatcher;
+use Hypervel\ExceptionHandler\ExceptionHandlerDispatcher;
+use Hypervel\HttpServer\ResponseEmitter;
+use Hypervel\Support\SafeCaller;
+use Hypervel\Tests\HttpServer\Stub\ServerStub;
+use Hypervel\Foundation\Testing\Concerns\RunTestsInCoroutine;
+use Hypervel\Tests\TestCase;
+use Mockery as m;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use RuntimeException;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class ServerTest extends TestCase
+{
+    use RunTestsInCoroutine;
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        CoordinatorManager::clear(Constants::WORKER_START);
+    }
+
+    public function testThrowExceptionInCatchOnRequest()
+    {
+        CoordinatorManager::until(Constants::WORKER_START)->resume();
+        $container = $this->getContainer();
+        $dispatcher = m::mock(ExceptionHandlerDispatcher::class);
+        $emitter = m::mock(ResponseEmitter::class);
+        $server = m::mock(ServerStub::class . '[initRequestAndResponse]', [
+            $container,
+            m::mock(HttpDispatcher::class),
+            $dispatcher,
+            $emitter,
+        ]);
+
+        $dispatcher->shouldReceive('dispatch')->andReturnUsing(function ($exception) {
+            throw new RuntimeException('Fatal Error');
+        });
+
+        $emitter->shouldReceive('emit')->once()->andReturnUsing(function ($response) {
+            $this->assertInstanceOf(Psr7Response::class, $response);
+            $this->assertSame(400, $response->getStatusCode());
+        });
+
+        $server->shouldReceive('initRequestAndResponse')->andReturnUsing(function () {
+            // Initialize PSR-7 Request and Response objects.
+            throw new BadRequestHttpException();
+        });
+
+        $server->onRequest($req = m::mock(Request::class), $res = m::mock(Response::class));
+    }
+
+    public function testOnRequest()
+    {
+        CoordinatorManager::until(Constants::WORKER_START)->resume();
+        $container = $this->getContainer();
+        $dispatcher = m::mock(ExceptionHandlerDispatcher::class);
+        $emitter = m::mock(ResponseEmitter::class);
+        $server = m::mock(ServerStub::class . '[initRequestAndResponse]', [
+            $container,
+            m::mock(HttpDispatcher::class),
+            $dispatcher,
+            $emitter,
+        ]);
+
+        $dispatcher->shouldReceive('dispatch')->andReturnUsing(function ($exception) {
+            if ($exception instanceof HttpException) {
+                return (new Psr7Response())->withStatus($exception->getStatusCode());
+            }
+            return null;
+        });
+
+        $emitter->shouldReceive('emit')->once()->andReturnUsing(function ($response) {
+            $this->assertInstanceOf(Psr7Response::class, $response);
+            $this->assertSame(400, $response->getStatusCode());
+        });
+
+        $server->shouldReceive('initRequestAndResponse')->andReturnUsing(function () {
+            // Initialize PSR-7 Request and Response objects.
+            throw new BadRequestHttpException();
+        });
+
+        $server->onRequest($req = m::mock(Request::class), $res = m::mock(Response::class));
+    }
+
+    protected function getContainer()
+    {
+        $container = m::mock(ContainerContract::class);
+        $container->shouldReceive('has')->with(ExceptionHandlerContract::class)->andReturnFalse();
+        $container->shouldReceive('get')->with(SafeCaller::class)->andReturn(new SafeCaller($container));
+
+        $dispatcher = m::mock(EventDispatcherInterface::class);
+        $dispatcher->shouldReceive('dispatch')->andReturn(true);
+        $container->shouldReceive('has')->with(EventDispatcherInterface::class)->andReturn(true);
+        $container->shouldReceive('get')->with(EventDispatcherInterface::class)->andReturn($dispatcher);
+
+        ApplicationContext::setContainer($container);
+
+        return $container;
+    }
+}
