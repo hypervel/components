731243cb4bbe852a265df86e9f667018ccb89dbc
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-15 11:23:35 +0000

    Port Server/Chunk/HasChunk.php to http-message package

---
+++ src/http-message/src/Server/Chunk/HasChunk.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\HttpMessage\Server\Chunk;
+
+use Hypervel\Contracts\Engine\Http\Writable;
+
+trait HasChunk
+{
+    /**
+     * Write a chunk of content to the connection.
+     */
+    public function write(string $content): bool
+    {
+        if (isset($this->connection) && $this->connection instanceof Writable) {
+            return $this->connection->write($content);
+        }
+
+        return false;
+    }
+}
