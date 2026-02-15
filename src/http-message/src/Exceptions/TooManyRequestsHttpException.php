2d7d3e6d34fffb0c8d16f0e9ff2df3e3d0e6b46f
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-15 11:20:26 +0000

    Port Exceptions/TooManyRequestsHttpException.php to http-message package
    Hypervel-only exception (429) with Retry-After header support, moved from core.

---
+++ src/http-message/src/Exceptions/TooManyRequestsHttpException.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\HttpMessage\Exceptions;
+
+use Throwable;
+
+class TooManyRequestsHttpException extends HttpException
+{
+    /**
+     * @param null|int|string $retryAfter The number of seconds or HTTP-date after which the request may be retried
+     */
+    public function __construct(
+        int|string|null $retryAfter = null,
+        string $message = '',
+        int $code = 0,
+        ?Throwable $previous = null,
+        array $headers = []
+    ) {
+        if ($retryAfter) {
+            $headers['Retry-After'] = $retryAfter;
+        }
+
+        parent::__construct(429, $message, $code, $previous, $headers);
+    }
+}
