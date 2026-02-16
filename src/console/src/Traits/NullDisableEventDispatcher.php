d35ca7281f6cbd98caa8f32204f26fda1b09a97b
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-16 16:48:27 +0000

    Port NullDisableEventDispatcher trait from Hyperf\Command to Hypervel\Console

---
+++ src/console/src/Traits/NullDisableEventDispatcher.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Console\Traits;
+
+use Symfony\Component\Console\Input\InputInterface;
+
+trait NullDisableEventDispatcher
+{
+    public function addDisableDispatcherOption(): void
+    {
+    }
+
+    public function disableDispatcher(InputInterface $input): void
+    {
+    }
+}
