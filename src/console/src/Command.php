c77dfa61ccd425c5a9fb1601fd39fa166a1798cc
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-16 16:58:29 +0000

    Merge Hyperf Command into Hypervel Console Command
    Make Hypervel\Console\Command extend Symfony's Command directly instead of
    Hyperf\Command\Command. Absorbs all properties, constructor logic, and methods
    from the Hyperf parent (run, call, createInputFromArguments, context,
    configureUsingFluentDefinition, configure, setUpTraits) while preserving all
    Hypervel-specific functionality (execute, isolation, replaceOutput, fail, etc.).

--- src/console/src/Command.php
+++ src/console/src/Command.php
@@ -6,7 +6,6 @@ namespace Hypervel\Console;
 
 use FriendsOfHyperf\CommandSignals\Traits\InteractsWithSignals;
 use FriendsOfHyperf\PrettyConsole\Traits\Prettyable;
-use Hyperf\Command\Command as HyperfCommand;
 use Hypervel\Console\Contracts\CommandMutex;
 use Hypervel\Console\Events\AfterExecute;
 use Hypervel\Console\Events\AfterHandle;
@@ -17,18 +16,61 @@ use Hypervel\Contracts\Console\Isolatable;
 use Hypervel\Contracts\Console\Kernel as KernelContract;
 use Hypervel\Contracts\Foundation\Application as ApplicationContract;
 use Hypervel\Coroutine\Coroutine;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Swoole\ExitException;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Throwable;
 
 use function Hypervel\Coroutine\run;
+use function Hypervel\Support\swoole_hook_flags;
 
-abstract class Command extends HyperfCommand
+abstract class Command extends SymfonyCommand
 {
     use InteractsWithSignals;
     use Prettyable;
+    use Traits\DisableEventDispatcher;
+    use Traits\HasParameters;
+    use Traits\InteractsWithIO;
+
+    /**
+     * The name of the command.
+     */
+    protected ?string $name = null;
+
+    /**
+     * The description of the command.
+     */
+    protected string $description = '';
+
+    /**
+     * Whether to execute in a coroutine environment.
+     */
+    protected bool $coroutine = true;
+
+    /**
+     * The event dispatcher instance.
+     */
+    protected ?EventDispatcherInterface $eventDispatcher = null;
+
+    /**
+     * The hook flags for the coroutine.
+     */
+    protected int $hookFlags = -1;
+
+    /**
+     * The name and signature of the command.
+     */
+    protected ?string $signature = null;
+
+    /**
+     * The exit code of the command.
+     */
+    protected int $exitCode = self::SUCCESS;
 
     protected ApplicationContract $app;
 
@@ -44,7 +86,27 @@ abstract class Command extends HyperfCommand
 
     public function __construct(?string $name = null)
     {
-        parent::__construct($name);
+        $this->name = $name ?? $this->name;
+
+        if ($this->hookFlags < 0) {
+            $this->hookFlags = swoole_hook_flags();
+        }
+
+        if (isset($this->signature)) {
+            $this->configureUsingFluentDefinition();
+        } else {
+            parent::__construct($this->name);
+        }
+
+        $this->addDisableDispatcherOption();
+
+        if (! empty($this->description)) {
+            $this->setDescription($this->description);
+        }
+
+        if (! isset($this->signature)) {
+            $this->specifyParameters();
+        }
 
         /* @phpstan-ignore assign.propertyType */
         $this->app = Container::getInstance();
@@ -52,6 +114,28 @@ abstract class Command extends HyperfCommand
         if ($this instanceof Isolatable) {
             $this->configureIsolation();
         }
+    }
+
+    /**
+     * Run the console command.
+     */
+    public function run(InputInterface $input, OutputInterface $output): int
+    {
+        $this->output = new SymfonyStyle($input, $output);
+
+        $this->setUpTraits($this->input = $input, $this->output);
+
+        return parent::run($this->input, $this->output);
+    }
+
+    /**
+     * Call another console command.
+     */
+    public function call(string $command, array $arguments = []): int
+    {
+        $arguments['command'] = $command;
+
+        return $this->getApplication()->find($command)->run($this->createInputFromArguments($arguments), $this->output);
     }
 
     /**
@@ -66,6 +150,55 @@ abstract class Command extends HyperfCommand
             'Do not run the command if another instance of the command is already running',
             $this->isolated
         ));
+    }
+
+    /**
+     * Configure the console command using a fluent definition.
+     */
+    protected function configureUsingFluentDefinition(): void
+    {
+        [$name, $arguments, $options] = Parser::parse($this->signature);
+
+        parent::__construct($this->name = $name);
+
+        // After parsing the signature we will spin through the arguments and options
+        // and set them on this command. These will already be changed into proper
+        // instances of these "InputArgument" and "InputOption" Symfony classes.
+        $this->getDefinition()->addArguments($arguments);
+        $this->getDefinition()->addOptions($options);
+    }
+
+    protected function configure(): void
+    {
+        parent::configure();
+    }
+
+    /**
+     * Create an input instance from the given arguments.
+     */
+    protected function createInputFromArguments(array $arguments): ArrayInput
+    {
+        return tap(new ArrayInput(array_merge($this->context(), $arguments)), function (InputInterface $input) {
+            if ($input->hasParameterOption(['--no-interaction'], true)) {
+                $input->setInteractive(false);
+            }
+        });
+    }
+
+    /**
+     * Get all the context passed to the command.
+     */
+    protected function context(): array
+    {
+        return collect($this->input->getOptions())->only([
+            'ansi',
+            'no-ansi',
+            'no-interaction',
+            'quiet',
+            'verbose',
+        ])->filter()->mapWithKeys(function ($value, $key) {
+            return ["--{$key}" => $value];
+        })->all();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -182,5 +315,21 @@ abstract class Command extends HyperfCommand
         return $this->app
             ->get(KernelContract::class)
             ->call($command, $arguments);
+    }
+
+    /**
+     * Set up the traits used by the command.
+     */
+    protected function setUpTraits(InputInterface $input, OutputInterface $output): array
+    {
+        $uses = array_flip(class_uses_recursive(static::class));
+
+        foreach ($uses as $trait) {
+            if (method_exists($this, $method = 'setUp' . class_basename($trait))) {
+                $this->{$method}($input, $output);
+            }
+        }
+
+        return $uses;
     }
 }
