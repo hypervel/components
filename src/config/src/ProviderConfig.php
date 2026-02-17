6efaa31a91342b6edf9b0c06e2fb9e68a6e237af
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-17 08:32:53 +0000

    Remove view-engine

--- src/config/src/ProviderConfig.php
+++ src/config/src/ProviderConfig.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Hypervel\Config;
 
-use Hyperf\Config\ProviderConfig as HyperfProviderConfig;
 use Hyperf\Di\Definition\PriorityDefinition;
 use Hypervel\Support\Arr;
 use Hypervel\Support\Composer;
