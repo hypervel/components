7be1769156eac84a3ea498397d5812290cdb15f1
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-23 19:58:17 +0000

    Add InteractsWithSignals trait for console commands
    Port of the FriendsOfHyperf trait providing trap()/untrap() methods
    for registering OS signal handlers in commands. Uses the new
    Hypervel\Console\SignalRegistry.

---
+++ src/console/src/Concerns/InteractsWithSignals.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Console\Concerns;
+
+use Hypervel\Console\SignalRegistry;
+use Hypervel\Container\Container;
+
+use function Hypervel\Coroutine\defer;
+
+trait InteractsWithSignals
+{
+    protected ?SignalRegistry $signalRegistry = null;
+
+    /**
+     * Define a callback to be run when the given signal(s) occurs.
+     *
+     * @param int|int[] $signo
+     * @param (callable(int $signo): void) $callback
+     */
+    protected function trap(array|int $signo, callable $callback): void
+    {
+        if (! $this->signalRegistry) {
+            $this->signalRegistry = Container::getInstance()->make(SignalRegistry::class);
+            defer(fn () => $this->signalRegistry->unregister());
+        }
+
+        $this->signalRegistry->register($signo, $callback);
+    }
+
+    /**
+     * Unregister signal handlers for one, many, or all signals.
+     *
+     * @param null|int|int[] $signo
+     */
+    protected function untrap(array|int|null $signo = null): void
+    {
+        $this->signalRegistry?->unregister($signo);
+    }
+}
