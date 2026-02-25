0e72c045b93dc56678ba95267a557b1311402889
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-02-25 12:47:20 +0000

    Fix code style

--- tests/Validation/ValidationEmailRuleTest.php
+++ tests/Validation/ValidationEmailRuleTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Hypervel\Tests\Validation;
 
-use Hypervel\Contracts\Translation\Translator as TranslatorContract;
 use Hypervel\Support\Arr;
 use Hypervel\Testbench\TestCase;
 use Hypervel\Translation\ArrayLoader;
