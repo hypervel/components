5b150fee172e9a4e4b4d5f91a7f22a0a4a0fb35b
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-03 21:07:23 +0000

    Rename tests/ExceptionHandler/Stub to Stubs

--- tests/ExceptionHandler/Stubs/BarExceptionHandler.php
+++ tests/ExceptionHandler/Stubs/BarExceptionHandler.php
@@ -2,18 +2,23 @@
 
 declare(strict_types=1);
 
-namespace Hypervel\Tests\ExceptionHandler\Stub;
+namespace Hypervel\Tests\ExceptionHandler\Stubs;
 
 use Hypervel\Context\Context;
 use Hypervel\ExceptionHandler\ExceptionHandler;
 use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
-class FooExceptionHandler extends ExceptionHandler
+class BarExceptionHandler extends ExceptionHandler
 {
     public function handle(Throwable $throwable, Response $response)
     {
         Context::set('test.exception-handler.latest-handler', static::class);
+
+        if ($throwable->getCode() === 0) {
+            $this->stopPropagation();
+        }
+
         return $response;
     }
 
--- tests/ExceptionHandler/Stubs/FooExceptionHandler.php
+++ tests/ExceptionHandler/Stubs/FooExceptionHandler.php
@@ -2,23 +2,18 @@
 
 declare(strict_types=1);
 
-namespace Hypervel\Tests\ExceptionHandler\Stub;
+namespace Hypervel\Tests\ExceptionHandler\Stubs;
 
 use Hypervel\Context\Context;
 use Hypervel\ExceptionHandler\ExceptionHandler;
 use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
-class BarExceptionHandler extends ExceptionHandler
+class FooExceptionHandler extends ExceptionHandler
 {
     public function handle(Throwable $throwable, Response $response)
     {
         Context::set('test.exception-handler.latest-handler', static::class);
-
-        if ($throwable->getCode() === 0) {
-            $this->stopPropagation();
-        }
-
         return $response;
     }
 
