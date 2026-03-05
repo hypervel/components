8934f0fba020d17d42cd104bab33f13b16878df8
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-05 19:11:38 +0000

    Add AspectManager tests
    Tests for runtime aspect cache: get, has, set, insert, and clear.

---
+++ tests/Di/Aop/AspectManagerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Tests\Di\Aop;
+
+use Hypervel\Di\Aop\AspectManager;
+use Hypervel\Tests\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class AspectManagerTest extends TestCase
+{
+    protected function tearDown(): void
+    {
+        AspectManager::clear();
+
+        parent::tearDown();
+    }
+
+    public function testHasReturnsFalseForUnsetEntry()
+    {
+        $this->assertFalse(AspectManager::has('Foo', 'bar'));
+    }
+
+    public function testSetAndGet()
+    {
+        AspectManager::set('Foo', 'bar', ['Aspect1', 'Aspect2']);
+
+        $this->assertTrue(AspectManager::has('Foo', 'bar'));
+        $this->assertSame(['Aspect1', 'Aspect2'], AspectManager::get('Foo', 'bar'));
+    }
+
+    public function testGetReturnsEmptyArrayForUnsetEntry()
+    {
+        $this->assertSame([], AspectManager::get('Foo', 'bar'));
+    }
+
+    public function testInsertAppendsToList()
+    {
+        AspectManager::set('Foo', 'bar', []);
+        AspectManager::insert('Foo', 'bar', 'Aspect1');
+        AspectManager::insert('Foo', 'bar', 'Aspect2');
+
+        $this->assertSame(['Aspect1', 'Aspect2'], AspectManager::get('Foo', 'bar'));
+    }
+
+    public function testClearRemovesAllEntries()
+    {
+        AspectManager::set('Foo', 'bar', ['Aspect1']);
+        AspectManager::set('Baz', 'qux', ['Aspect2']);
+
+        AspectManager::clear();
+
+        $this->assertFalse(AspectManager::has('Foo', 'bar'));
+        $this->assertFalse(AspectManager::has('Baz', 'qux'));
+    }
+}
