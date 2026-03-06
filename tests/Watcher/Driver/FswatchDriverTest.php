b5c145f3d86a37f04e8f21befe8efc2ab5b546a4
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-06 03:55:30 +0000

    Add FswatchDriver with test and stub

---
+++ tests/Watcher/Driver/FswatchDriverTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Tests\Watcher\Driver;
+
+use Hypervel\Engine\Channel;
+use Hypervel\Foundation\Testing\Concerns\RunTestsInCoroutine;
+use Hypervel\Tests\TestCase;
+use Hypervel\Tests\Watcher\Stubs\ContainerStub;
+use Hypervel\Tests\Watcher\Stubs\FswatchDriverStub;
+use Hypervel\Watcher\Driver\FswatchDriver;
+use Hypervel\Watcher\Option;
+use InvalidArgumentException;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class FswatchDriverTest extends TestCase
+{
+    use RunTestsInCoroutine;
+
+    public function testWatch()
+    {
+        $container = ContainerStub::getContainer(FswatchDriver::class);
+        $option = new Option($container->make('config')->get('watcher'), [], []);
+        $channel = new Channel(10);
+
+        try {
+            $driver = new FswatchDriverStub($option);
+            $driver->watch($channel);
+
+            $this->assertSame('.env', $channel->pop($option->getScanIntervalSeconds() + 0.1));
+        } catch (InvalidArgumentException $e) {
+            if (str_contains($e->getMessage(), 'fswatch not exists')) {
+                $this->markTestSkipped();
+            }
+            throw $e;
+        } finally {
+            if (isset($driver)) {
+                $driver->stop();
+            }
+            $channel->close();
+        }
+    }
+}
