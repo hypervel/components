9261faeb0f6904b8155a19eb907df558f9d4dfeb
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-09 07:07:55 +0000

    Port PreviousUrlTest from Laravel

---
+++ tests/Integration/Routing/PreviousUrlTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Tests\Integration\Routing\PreviousUrlTest;
+
+use Hypervel\Contracts\Foundation\Application as ApplicationContract;
+use Hypervel\Foundation\Http\FormRequest;
+use Hypervel\Session\SessionServiceProvider;
+use Hypervel\Support\Facades\Route;
+use Hypervel\Tests\Integration\Routing\RoutingTestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class PreviousUrlTest extends RoutingTestCase
+{
+    public function testPreviousUrlWithoutSession()
+    {
+        Route::post('/previous-url', function (DummyFormRequest $request) {
+            return 'OK';
+        });
+
+        $response = $this->postJson('/previous-url');
+
+        $this->assertEquals(422, $response->status());
+    }
+
+    protected function getApplicationProviders(ApplicationContract $app): array
+    {
+        $providers = parent::getApplicationProviders($app);
+
+        return array_filter($providers, function ($provider) {
+            return $provider !== SessionServiceProvider::class;
+        });
+    }
+}
+
+class DummyFormRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'foo' => [
+                'required',
+                'string',
+            ],
+        ];
+    }
+}
