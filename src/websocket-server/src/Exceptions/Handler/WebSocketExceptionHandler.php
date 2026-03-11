34a43fe4cf420bf1661f64f230fb75841c11651a
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-11 04:13:59 +0000

    Decouple WebSocketExceptionHandler from Hyperf ExceptionHandler

--- src/websocket-server/src/Exceptions/Handler/WebSocketExceptionHandler.php
+++ src/websocket-server/src/Exceptions/Handler/WebSocketExceptionHandler.php
@@ -5,16 +5,15 @@ declare(strict_types=1);
 namespace Hypervel\WebSocketServer\Exceptions\Handler;
 
 use Hypervel\Contracts\Log\StdoutLoggerInterface;
-use Hypervel\ExceptionHandler\ExceptionHandler;
-use Hypervel\ExceptionHandler\Formatter\FormatterInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
-class WebSocketExceptionHandler extends ExceptionHandler
+class WebSocketExceptionHandler
 {
-    public function __construct(protected StdoutLoggerInterface $logger, protected FormatterInterface $formatter)
-    {
+    public function __construct(
+        protected StdoutLoggerInterface $logger,
+    ) {
     }
 
     /**
@@ -22,20 +21,12 @@ class WebSocketExceptionHandler extends ExceptionHandler
      */
     public function handle(Throwable $throwable, Response $response): Response
     {
-        $this->logger->warning($this->formatter->format($throwable));
+        $this->logger->warning((string) $throwable);
 
         $statusCode = $throwable instanceof HttpException
             ? $throwable->getStatusCode()
             : Response::HTTP_INTERNAL_SERVER_ERROR;
 
         return new Response($throwable->getMessage(), $statusCode);
-    }
-
-    /**
-     * Determine if this handler should handle the given exception.
-     */
-    public function isValid(Throwable $throwable): bool
-    {
-        return true;
     }
 }
