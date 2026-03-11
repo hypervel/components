d9a26c77afc64c51ba461b0a836287adeeb3626f
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-11 04:13:21 +0000

    Add HandleExceptions to console kernel bootstrappers

--- src/foundation/src/Console/Kernel.php
+++ src/foundation/src/Console/Kernel.php
@@ -88,6 +88,7 @@ class Kernel implements KernelContract
     protected array $bootstrappers = [
         \Hypervel\Foundation\Bootstrap\LoadEnvironmentVariables::class,
         \Hypervel\Foundation\Bootstrap\LoadConfiguration::class,
+        \Hypervel\Foundation\Bootstrap\HandleExceptions::class,
         \Hypervel\Foundation\Bootstrap\RegisterFacades::class,
         \Hypervel\Foundation\Bootstrap\RegisterProviders::class,
         \Hypervel\Di\Bootstrap\GenerateProxies::class,
