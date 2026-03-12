68fbe9fba6a331aa0140dfdc6014113362c3ef2e
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-12 16:20:11 +0000

    Update Cookie facade phpdoc for nullable params and new contract

--- src/support/src/Facades/Cookie.php
+++ src/support/src/Facades/Cookie.php
@@ -7,15 +7,15 @@ namespace Hypervel\Support\Facades;
 /**
  * @method static bool has(\UnitEnum|string $key)
  * @method static string|null get(\UnitEnum|string $key, string|null $default = null)
- * @method static \Hypervel\Cookie\Cookie make(\UnitEnum|string $name, string $value, int $minutes = 0, string $path = '', string $domain = '', bool $secure = false, bool $httpOnly = true, bool $raw = false, string|null $sameSite = null)
+ * @method static \Hypervel\Cookie\Cookie make(\UnitEnum|string $name, string $value, int $minutes = 0, string|null $path = null, string|null $domain = null, bool|null $secure = null, bool $httpOnly = true, bool $raw = false, string|null $sameSite = null)
  * @method static void queue(mixed ...$parameters)
- * @method static void expire(\UnitEnum|string $name, string $path = '', string $domain = '')
- * @method static void unqueue(\UnitEnum|string $name, string $path = '')
+ * @method static void expire(\UnitEnum|string $name, string|null $path = null, string|null $domain = null)
+ * @method static void unqueue(\UnitEnum|string $name, string|null $path = null)
  * @method static array getQueuedCookies()
- * @method static \Hypervel\Cookie\Cookie forever(\UnitEnum|string $name, string $value, string $path = '', string $domain = '', bool $secure = false, bool $httpOnly = true, bool $raw = false, string|null $sameSite = null)
- * @method static \Hypervel\Cookie\Cookie forget(\UnitEnum|string $name, string $path = '', string $domain = '')
+ * @method static \Hypervel\Cookie\Cookie forever(\UnitEnum|string $name, string $value, string|null $path = null, string|null $domain = null, bool|null $secure = null, bool $httpOnly = true, bool $raw = false, string|null $sameSite = null)
+ * @method static \Hypervel\Cookie\Cookie forget(\UnitEnum|string $name, string|null $path = null, string|null $domain = null)
  *
- * @see \Hypervel\Cookie\CookieManager
+ * @see \Hypervel\Cookie\CookieJar
  */
 class Cookie extends Facade
 {
