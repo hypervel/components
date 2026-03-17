a539eacd1893e672775e2925f5da9d91452fe094
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-17 06:33:17 +0000

    Remove dead shortname check from ServerStartCommand
    The check was guarded by Composer::hasPackage('hypervel/polyfill-coroutine')
    which doesn't exist, so it never ran. The shortname validation is now
    handled by Application::checkEnvironment() which covers all entry points.

--- src/server/src/Commands/ServerStartCommand.php
+++ src/server/src/Commands/ServerStartCommand.php
@@ -11,7 +11,6 @@ use Hypervel\Contracts\Log\StdoutLoggerInterface;
 use Hypervel\Engine\Coroutine;
 use Hypervel\Foundation\Application;
 use Hypervel\Server\ServerFactory;
-use Hypervel\Support\Composer;
 use InvalidArgumentException;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -34,8 +33,6 @@ class ServerStartCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->checkEnvironment($output);
-
         Application::getInstance()->setRunningInConsole(false);
 
         $serverFactory = $this->container->make(ServerFactory::class)
@@ -54,44 +51,5 @@ class ServerStartCommand extends Command
         $serverFactory->start();
 
         return 0;
-    }
-
-    /**
-     * Check if the Swoole environment is properly configured.
-     */
-    private function checkEnvironment(OutputInterface $output): void
-    {
-        if (! extension_loaded('swoole') || ! Composer::hasPackage('hypervel/polyfill-coroutine')) {
-            return;
-        }
-        /**
-         * swoole.use_shortname = true       => string(1) "1"     => enabled
-         * swoole.use_shortname = "true"     => string(1) "1"     => enabled
-         * swoole.use_shortname = on         => string(1) "1"     => enabled
-         * swoole.use_shortname = On         => string(1) "1"     => enabled
-         * swoole.use_shortname = "On"       => string(2) "On"    => enabled
-         * swoole.use_shortname = "on"       => string(2) "on"    => enabled
-         * swoole.use_shortname = 1          => string(1) "1"     => enabled
-         * swoole.use_shortname = "1"        => string(1) "1"     => enabled
-         * swoole.use_shortname = 2          => string(1) "1"     => enabled
-         * swoole.use_shortname = false      => string(0) ""      => disabled
-         * swoole.use_shortname = "false"    => string(5) "false" => disabled
-         * swoole.use_shortname = off        => string(0) ""      => disabled
-         * swoole.use_shortname = Off        => string(0) ""      => disabled
-         * swoole.use_shortname = "off"      => string(3) "off"   => disabled
-         * swoole.use_shortname = "Off"      => string(3) "Off"   => disabled
-         * swoole.use_shortname = 0          => string(1) "0"     => disabled
-         * swoole.use_shortname = "0"        => string(1) "0"     => disabled
-         * swoole.use_shortname = 00         => string(2) "00"    => disabled
-         * swoole.use_shortname = "00"       => string(2) "00"    => disabled
-         * swoole.use_shortname = ""         => string(0) ""      => disabled
-         * swoole.use_shortname = " "        => string(1) " "     => disabled.
-         */
-        $useShortname = ini_get_all('swoole')['swoole.use_shortname']['local_value'];
-        $useShortname = strtolower(trim(str_replace('0', '', $useShortname)));
-        if (! in_array($useShortname, ['', 'off', 'false'], true)) {
-            $output->writeln("<error>ERROR</error> Swoole short function names must be disabled before the server starts, please set swoole.use_shortname='Off' in your php.ini.");
-            exit(SIGTERM);
-        }
     }
 }
