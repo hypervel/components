ebfe4cff130bfc9454aa21317db5f29557312600
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-18 00:22:41 +0000

    Add auth Login, Logout, and OtherDeviceLogout events

---
+++ src/auth/src/Events/OtherDeviceLogout.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Auth\Events;
+
+use Hypervel\Contracts\Auth\Authenticatable;
+use Hypervel\Queue\SerializesModels;
+
+class OtherDeviceLogout
+{
+    use SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param string $guard the authentication guard name
+     * @param Authenticatable $user the authenticated user
+     */
+    public function __construct(
+        public string $guard,
+        public Authenticatable $user,
+    ) {
+    }
+}
