9f4b122a3e532f2be434f3e97ca152b75d429f43
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-18 00:21:35 +0000

    Port Authenticatable contract: add password name, remember token methods

--- src/contracts/src/Auth/Authenticatable.php
+++ src/contracts/src/Auth/Authenticatable.php
@@ -17,7 +17,27 @@ interface Authenticatable
     public function getAuthIdentifier(): mixed;
 
     /**
+     * Get the name of the password attribute for the user.
+     */
+    public function getAuthPasswordName(): string;
+
+    /**
      * Get the password for the user.
      */
-    public function getAuthPassword(): string;
+    public function getAuthPassword(): ?string;
+
+    /**
+     * Get the token value for the "remember me" session.
+     */
+    public function getRememberToken(): ?string;
+
+    /**
+     * Set the token value for the "remember me" session.
+     */
+    public function setRememberToken(string $value): void;
+
+    /**
+     * Get the column name for the "remember me" token.
+     */
+    public function getRememberTokenName(): string;
 }
