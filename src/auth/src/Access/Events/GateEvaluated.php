0278d7a71e3223bbf6d8d11a7eb0c2c7c939d0f4
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-18 00:22:32 +0000

    Port GateEvaluated event

--- src/auth/src/Access/Events/GateEvaluated.php
+++ src/auth/src/Access/Events/GateEvaluated.php
@@ -5,23 +5,22 @@ declare(strict_types=1);
 namespace Hypervel\Auth\Access\Events;
 
 use Hypervel\Auth\Access\Response;
-use Hypervel\Contracts\Auth\Authenticatable;
 
 class GateEvaluated
 {
     /**
      * Create a new event instance.
      *
-     * @param null|Authenticatable $user the authenticatable model
+     * @param mixed $user the user being evaluated
      * @param string $ability the ability being evaluated
      * @param null|bool|Response $result the result of the evaluation
      * @param array $arguments the arguments given during evaluation
      */
     public function __construct(
-        public ?Authenticatable $user,
+        public mixed $user,
         public string $ability,
         public bool|Response|null $result,
-        public array $arguments
+        public array $arguments,
     ) {
     }
 }
