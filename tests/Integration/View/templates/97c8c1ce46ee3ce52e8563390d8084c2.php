070950d1a2edac5ac28728ce6390bf19dcf5d931
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-20 05:11:51 +0000

    Add view integration test templates

---
+++ tests/Integration/View/templates/97c8c1ce46ee3ce52e8563390d8084c2.php
@@ -0,0 +1,4 @@
+<?php $__env->startSection('title', __('Page Expired')); ?>
+<?php $__env->startSection('code', '419'); ?>
+<?php $__env->startSection('message', __('Page Expired')); ?>
+<?php echo $__env->make('errors::minimal', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /* PATH /home/binaryfire/workspace/monorepo/contrib/hypervel/components/src/foundation/src/Exceptions/views/419.blade.php ENDPATH */ ?>
