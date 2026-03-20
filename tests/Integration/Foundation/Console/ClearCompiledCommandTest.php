95c7314b27a1c36cbc86e2a2d1a0a91d4ca11333
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-20 00:42:05 +0000

    Add ClearCompiledCommand tests

---
+++ tests/Integration/Foundation/Console/ClearCompiledCommandTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Tests\Integration\Foundation\Console;
+
+use Hypervel\Testbench\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class ClearCompiledCommandTest extends TestCase
+{
+    public function testDeletesCachedServicesFile()
+    {
+        $path = $this->app->getCachedServicesPath();
+        $dir = dirname($path);
+
+        if (! is_dir($dir)) {
+            mkdir($dir, 0755, true);
+        }
+
+        file_put_contents($path, '<?php return [];');
+        $this->assertFileExists($path);
+
+        $this->artisan('clear-compiled')->assertSuccessful();
+
+        $this->assertFileDoesNotExist($path);
+    }
+
+    public function testDeletesCachedPackagesFile()
+    {
+        $path = $this->app->getCachedPackagesPath();
+        $dir = dirname($path);
+
+        if (! is_dir($dir)) {
+            mkdir($dir, 0755, true);
+        }
+
+        file_put_contents($path, '<?php return [];');
+        $this->assertFileExists($path);
+
+        $this->artisan('clear-compiled')->assertSuccessful();
+
+        $this->assertFileDoesNotExist($path);
+    }
+
+    public function testSucceedsWhenNoFilesExist()
+    {
+        @unlink($this->app->getCachedServicesPath());
+        @unlink($this->app->getCachedPackagesPath());
+
+        $this->artisan('clear-compiled')->assertSuccessful();
+    }
+}
