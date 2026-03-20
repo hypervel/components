4d0be211645509d8d2f916ac67107d3a751d3414
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-20 00:36:17 +0000

    Add CanResetPassword and MustVerifyEmail traits to Foundation User

--- src/foundation/src/Auth/User.php
+++ src/foundation/src/Auth/User.php
@@ -5,13 +5,18 @@ declare(strict_types=1);
 namespace Hypervel\Foundation\Auth;
 
 use Hypervel\Auth\Authenticatable;
+use Hypervel\Auth\MustVerifyEmail;
+use Hypervel\Auth\Passwords\CanResetPassword;
 use Hypervel\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Hypervel\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Hypervel\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Hypervel\Database\Eloquent\Model;
 use Hypervel\Foundation\Auth\Access\Authorizable;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends Model implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
 {
     use Authenticatable;
     use Authorizable;
+    use CanResetPassword;
+    use MustVerifyEmail;
 }
