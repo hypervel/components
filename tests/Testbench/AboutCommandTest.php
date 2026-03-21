4b5f8c4596138098662a822f50cebd95b83cb28d
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-21 17:45:54 +0000

    Add test for AboutCommand testbench info section

---
+++ tests/Testbench/AboutCommandTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Tests\Testbench;
+
+use Hypervel\Testbench\Concerns\Database\InteractsWithSqliteDatabaseFile;
+use Hypervel\Testbench\TestCase;
+use PHPUnit\Framework\Attributes\RequiresOperatingSystem;
+use PHPUnit\Framework\Attributes\Test;
+
+use function Hypervel\Testbench\remote;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+#[RequiresOperatingSystem('Linux|Darwin')]
+class AboutCommandTest extends TestCase
+{
+    use InteractsWithSqliteDatabaseFile;
+
+    #[Test]
+    public function itIncludesTestbenchInformationInTheAboutCommandOutput()
+    {
+        $this->withoutSqliteDatabase(function (): void {
+            $process = remote('about --json');
+            $process->mustRun();
+
+            $output = json_decode($process->getOutput(), true);
+
+            $this->assertIsArray($output);
+            $this->assertArrayHasKey('testbench', $output);
+            $this->assertSame(BASE_PATH, $output['testbench']['skeleton_path']);
+        });
+    }
+}
