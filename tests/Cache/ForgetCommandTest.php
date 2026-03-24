539f0433315a836ff374928fb0140fc0028a2a7f
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-03-24 10:23:00 +0000

    Add ForgetCommand test
    Tests cache:forget command with default and specified store, asserting
    both exit code and output message.

---
+++ tests/Cache/ForgetCommandTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Tests\Cache;
+
+use Hypervel\Cache\CacheManager;
+use Hypervel\Cache\Console\ForgetCommand;
+use Hypervel\Contracts\Cache\Repository;
+use Hypervel\Foundation\Application;
+use Hypervel\Tests\TestCase;
+use Mockery as m;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class ForgetCommandTest extends TestCase
+{
+    public function testForgetKeyFromDefaultStore()
+    {
+        $cacheManager = m::mock(CacheManager::class);
+        $repository = m::mock(Repository::class);
+
+        $cacheManager->shouldReceive('store')->once()->with(null)->andReturn($repository);
+        $repository->shouldReceive('forget')->once()->with('my-key');
+
+        $command = new ForgetCommand($cacheManager);
+        $command->setHypervel(new Application());
+
+        $output = new BufferedOutput();
+
+        $this->assertSame(0, $command->run(new ArrayInput(['key' => 'my-key']), $output));
+        $this->assertStringContainsString(
+            'key has been removed from the cache',
+            $output->fetch()
+        );
+    }
+
+    public function testForgetKeyFromSpecifiedStore()
+    {
+        $cacheManager = m::mock(CacheManager::class);
+        $repository = m::mock(Repository::class);
+
+        $cacheManager->shouldReceive('store')->once()->with('redis')->andReturn($repository);
+        $repository->shouldReceive('forget')->once()->with('my-key');
+
+        $command = new ForgetCommand($cacheManager);
+        $command->setHypervel(new Application());
+
+        $output = new BufferedOutput();
+
+        $this->assertSame(0, $command->run(new ArrayInput(['key' => 'my-key', 'store' => 'redis']), $output));
+        $this->assertStringContainsString(
+            'key has been removed from the cache',
+            $output->fetch()
+        );
+    }
+}
