f1c8145b3b2c886c52a338abd9cf8c65323a0376
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-04-03 02:39:32 +0000

    Define core reverb contracts

---
+++ src/reverb/src/Contracts/WebSocketConnection.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Reverb\Contracts;
+
+interface WebSocketConnection
+{
+    /**
+     * Get the raw socket connection identifier.
+     */
+    public function id(): int|string;
+
+    /**
+     * Send a message to the connection.
+     */
+    public function send(mixed $message): void;
+
+    /**
+     * Send a control frame to the connection.
+     */
+    public function control(int $opcode): void;
+
+    /**
+     * Close the connection.
+     */
+    public function close(mixed $message = null): void;
+}
