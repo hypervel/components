87428610cb8cf9ca047a0c05f925b1e0539be748
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-04-03 02:39:00 +0000

    Include Reverb WebSocketHandler in test cleanup subscriber

--- tests/AfterEachTestSubscriber.php
+++ tests/AfterEachTestSubscriber.php
@@ -109,6 +109,7 @@ final class AfterEachTestSubscriber implements FinishedSubscriber
         \Hypervel\Prompts\Prompt::flushState();
         \Hypervel\Queue\Console\WorkCommand::flushState();
         \Hypervel\Queue\Queue::createPayloadUsing(null);
+        \Hypervel\Reverb\Servers\Hypervel\WebSocketHandler::flushState();
         \Hypervel\Routing\CallableDispatcher::flushState();
         \Hypervel\Routing\CompiledRouteCollection::flushCache();
         \Hypervel\Routing\ControllerDispatcher::flushState();
