a7001625b60ce85eac64dda1990182f8625a3010
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-04-03 02:39:58 +0000

    Port reverb concerns

---
+++ src/reverb/src/Concerns/GeneratesIdentifiers.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Reverb\Concerns;
+
+trait GeneratesIdentifiers
+{
+    /**
+     * Generate a Pusher-compatible socket ID.
+     */
+    protected function generateId(): string
+    {
+        return sprintf('%d.%d', random_int(1, 1_000_000_000), random_int(1, 1_000_000_000));
+    }
+}
