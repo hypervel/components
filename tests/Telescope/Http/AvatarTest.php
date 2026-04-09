bf5bcebea21a245a5b65da4d0100069c81c202ae
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-04-09 16:49:25 +0000

    Align and strengthen AvatarTest with Laravel
    Adds testItCanGenerateAvatarUrl for default Gravatar URL generation.
    Adds testItCanReadCustomAvatarPathOnNullEmail for null email edge case.
    Removes redundant createUsersTable calls.

--- tests/Telescope/Http/AvatarTest.php
+++ tests/Telescope/Http/AvatarTest.php
@@ -6,6 +6,7 @@ namespace Hypervel\Tests\Telescope\Http;
 
 use Hypervel\Contracts\Auth\Authenticatable;
 use Hypervel\Database\Eloquent\Model;
+use Hypervel\Support\Str;
 use Hypervel\Telescope\Http\Middleware\Authorize;
 use Hypervel\Telescope\Telescope;
 use Hypervel\Telescope\Watchers\LogWatcher;
@@ -30,20 +31,93 @@ class AvatarTest extends FeatureTestCase
         $this->withoutMiddleware(Authorize::class);
     }
 
+    public function testItCanGenerateAvatarUrl()
+    {
+        $user = null;
+
+        Telescope::withoutRecording(function () use (&$user) {
+            $user = UserEloquent::create([
+                'id' => 1,
+                'name' => 'Telescope',
+                'email' => 'telescope@hypervel.org',
+                'password' => 'secret',
+            ]);
+        });
+
+        $this->actingAs($user);
+
+        $this->app->make(LoggerInterface::class)
+            ->error('Avatar path will be generated.', [
+                'exception' => 'Some error message',
+            ]);
+
+        $entry = $this->loadTelescopeEntries()->first();
+
+        $this->get("/telescope/telescope-api/logs/{$entry->uuid}")
+            ->assertOk()
+            ->assertJson([
+                'entry' => [
+                    'content' => [
+                        'user' => [
+                            'avatar' => 'https://www.gravatar.com/avatar/' . md5(Str::lower($user['email'])) . '?s=200',
+                        ],
+                    ],
+                ],
+            ]);
+    }
+
     public function testItCanRegisterCustomAvatarPath()
     {
         $user = null;
 
         Telescope::withoutRecording(function () use (&$user) {
-            $this->createUsersTable();
-
             $user = UserEloquent::create([
                 'id' => 1,
                 'name' => 'Telescope',
-                'email' => 'telescope@laravel.com',
+                'email' => 'telescope@hypervel.org',
                 'password' => 'secret',
             ]);
         });
+
+        Telescope::avatar(function ($id) {
+            return "/images/{$id}.jpg";
+        });
+
+        $this->actingAs($user);
+
+        $this->app->make(LoggerInterface::class)
+            ->error('Avatar path will be generated.', [
+                'exception' => 'Some error message',
+            ]);
+
+        $entry = $this->loadTelescopeEntries()->first();
+
+        $this->get("/telescope/telescope-api/logs/{$entry->uuid}")
+            ->assertOk()
+            ->assertJson([
+                'entry' => [
+                    'content' => [
+                        'user' => [
+                            'avatar' => '/images/1.jpg',
+                        ],
+                    ],
+                ],
+            ]);
+    }
+
+    public function testItCanReadCustomAvatarPathOnNullEmail()
+    {
+        $user = null;
+
+        Telescope::withoutRecording(function () use (&$user) {
+            $user = UserEloquent::create([
+                'id' => 1,
+                'name' => 'Telescope',
+                'email' => 'telescope@hypervel.org',
+                'password' => 'secret',
+            ]);
+        });
+        $user->email = null;
 
         Telescope::avatar(function ($id) {
             return "/images/{$id}.jpg";
