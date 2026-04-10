20bb1d5c8ab35e9136a1caced7428d0efda4b1c4
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-04-10 13:24:04 +0000

    Add ReverbController and API routes to Telescope

--- src/telescope/routes/web.php
+++ src/telescope/routes/web.php
@@ -78,6 +78,10 @@ Route::get('/telescope-api/redis/{id}', 'RedisController@show');
 Route::post('/telescope-api/client-requests', 'ClientRequestController@index');
 Route::get('/telescope-api/client-requests/{id}', 'ClientRequestController@show');
 
+// Reverb entries...
+Route::post('/telescope-api/reverb', 'ReverbController@index');
+Route::get('/telescope-api/reverb/{id}', 'ReverbController@show');
+
 // Monitored Tags...
 Route::get('/telescope-api/monitored-tags', 'MonitoredTagController@index');
 Route::post('/telescope-api/monitored-tags/', 'MonitoredTagController@store');
