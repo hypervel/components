b8b761ab791f49dff3d69a51ba06b05b442d3680
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-04-12 19:43:26 +0000

    Add ChannelListCommandTest
    Verifies the stale BroadcastServiceProvider warning is no longer
    emitted and the empty channels error message works correctly.

---
+++ tests/Foundation/Console/ChannelListCommandTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Tests\Foundation\Console;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class ChannelListCommandTest extends \Hypervel\Testbench\TestCase
+{
+    public function testDoesNotWarnAboutBroadcastServiceProvider()
+    {
+        $this->artisan('channel:list')
+            ->doesntExpectOutputToContain('BroadcastServiceProvider')
+            ->assertSuccessful();
+    }
+
+    public function testOutputsErrorWhenNoChannelsRegistered()
+    {
+        $this->artisan('channel:list')
+            ->expectsOutputToContain("Your application doesn't have any private broadcasting channels.")
+            ->assertSuccessful();
+    }
+}
