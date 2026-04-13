8881b193f0b435ef7ff942ecbf1c244e70b25c86
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-04-13 12:59:54 +0000

    Add Inertia provider interfaces

---
+++ src/inertia/src/ProvidesInertiaProperty.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypervel\Inertia;
+
+interface ProvidesInertiaProperty
+{
+    /**
+     * Convert the instance to an Inertia property value. This method is called
+     * when the object is used as a property value in an Inertia response,
+     * allowing for custom serialization.
+     */
+    public function toInertiaProperty(PropertyContext $prop): mixed;
+}
