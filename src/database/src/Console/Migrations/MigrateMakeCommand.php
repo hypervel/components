f7818f2582eaceccfff88865249f4c93c17b111f
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-07-12 17:42:12 +0000

    Remove a stale migration autoload comment
    Migration creation no longer invokes Composer or dumps autoload files, but the surrounding comment still described that removed behavior.
    
    Delete the obsolete comment instead of replacing it with narration of the self-explanatory writeMigration call.

--- src/database/src/Console/Migrations/MigrateMakeCommand.php
+++ src/database/src/Console/Migrations/MigrateMakeCommand.php
@@ -76,9 +76,6 @@ class MigrateMakeCommand extends BaseCommand implements PromptsForMissingInput
             [$table, $create] = TableGuesser::guess($name);
         }
 
-        // Now we are ready to write the migration out to disk. Once we've written
-        // the migration out, we will dump-autoload for the entire framework to
-        // make sure that the migrations are registered by the class loaders.
         $this->writeMigration($name, $table, $create);
     }
 
